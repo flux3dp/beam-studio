5e26b997d9a23d1f4f052458c2408b4dcc6ef77f
Simon Ko <simonxeko@gmail.com>
2019-08-14 17:28:17 +0800

    Update notarize.js

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -3,16 +3,17 @@ exports.default = async function notarizing(context) {
   if (electronPlatformName !== 'darwin') {
     return;
   }
-  console.log("Begin Notarizing!!!");
+  console.log("==== Begin Notarizing!!! ====");
   require('dotenv').config();
   const { notarize } = require('electron-notarize');
 
   const appName = context.packager.appInfo.productFilename;
 
-  return await notarize({
+  let result = await notarize({
     appBundleId: 'com.flux3dp.beam-studio',
     appPath: `${appOutDir}/${appName}.app`,
     appleId: process.env.APPLEID,
     appleIdPassword: process.env.APPLEIDPASS,
   });
+  console.log("==== Notarize Done. ====");
 };
