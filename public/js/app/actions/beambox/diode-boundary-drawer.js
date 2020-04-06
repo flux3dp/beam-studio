e3bb512bdeb5db4bdca0576ce09f2a85d0287e39
dean sung <a71124725@gmail.com>
2020-04-06 18:03:45 +0800

    Fix diode boundary region

--- public/js/app/actions/beambox/diode-boundary-drawer.js
+++ public/js/app/actions/beambox/diode-boundary-drawer.js
@@ -33,13 +33,13 @@ define([
         if (!diodeBoundaryPath) createBoundary();
         const w = Constant.dimension.getWidth();
         const h = Constant.dimension.getHeight();
-        const limitX1 = Constant.diode.limitX * Constant.dpmm;
-        const limitY1 = Constant.diode.limitY * Constant.dpmm;
+        const limitXR = Constant.diode.limitX * Constant.dpmm;
+        const limitYB = Constant.diode.limitY * Constant.dpmm;
         const OffsetX = BeamboxPreference.read('diode_offset_x');
         const OffsetY = BeamboxPreference.read('diode_offset_y');
-        const limitX2 = (OffsetX !== undefined ? OffsetX : Constant.diode.defaultOffsetX)* Constant.dpmm;
-        const limitY2 = (OffsetY !== undefined ? OffsetY : Constant.diode.defaultOffsetY)* Constant.dpmm;
-        const d = `M${w},${h}L0,${h}L0,0L${w},${0}zM${limitX1},${limitY1}L${w - limitX2},${limitY1}L${w - limitX2},${h - limitY2}L${limitX1},${h - limitY2}z`;
+        const limitXL = (OffsetX !== undefined ? OffsetX : Constant.diode.defaultOffsetX)* Constant.dpmm;
+        const limitYT = (OffsetY !== undefined ? OffsetY : Constant.diode.defaultOffsetY)* Constant.dpmm;
+        const d = `M${w},${h}L0,${h}L0,0L${w},${0}zM${limitXL},${limitYT}L${w - limitXR},${limitYT}L${w - limitXR},${h - limitYB}L${limitXL},${h - limitYB}z`;
         $(diodeBoundaryPath).attr('d', d);
     };
     const hide = () => {
