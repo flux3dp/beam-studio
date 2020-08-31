1275d516ac48a9d9e082838f742300789485984c
Simon Ko <simonxeko@gmail.com>
2020-08-31 14:21:50 +0800

    Fix ToolPanelsController ref in svgeditor select

--- public/js/lib/svgeditor/select.js
+++ public/js/lib/svgeditor/select.js
@@ -16,9 +16,12 @@
 // 4) math.js
 // 5) svgutils.js
 define([
+	'app/actions/beambox/Tool-Panels-Controller'
 ], function(
+	ToolPanelsController
 ){
 'use strict';
+ToolPanelsController = ToolPanelsController.default;
 
 if (!svgedit.select) {
 	svgedit.select = {};
