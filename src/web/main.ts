7d722c32e48ad0c861a35498b59dcde8b535f9b7
Dean Sung <a71124725@gmail.com>
2021-04-23 16:44:37 +0800

    Add electron to Window interface

--- src/web/main.ts
+++ src/web/main.ts
@@ -7,21 +7,29 @@ import globalEvents from './app/actions/global';
 import menuBar from './helpers/menubar';
 
 declare global {
-    var requireNode: (name: string) => any
+  var requireNode: (name: string) => any
+  interface Window {
+    electron: {
+      ipc: any,
+      events: { [key: string]: string; },
+      version: string,
+      trigger_file_input_click: (inputId: string) => void,
+    },
+  }
 }
 
-export default function() {
-    console.log(`Beam-Studio: ${window['FLUX'].version}`);
+export default function () {
+  console.log(`Beam-Studio: ${window['FLUX'].version}`);
 
-    if(allowTracking) {
-        // google analytics
-        $.getScript('/js/helpers/analytics.js');
-    }
+  if (allowTracking) {
+    // google analytics
+    $.getScript('/js/helpers/analytics.js');
+  }
 
-    menuBar();
+  menuBar();
 
-    globalEvents(function() {
-        let router = new Router();
-        Backbone.history.start();
-    });
+  globalEvents(function () {
+    let router = new Router();
+    Backbone.history.start();
+  });
 }
