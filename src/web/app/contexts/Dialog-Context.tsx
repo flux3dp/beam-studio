0c720a391c62a3a23bf478ecc0c3b926e08d5c77
Net Tseng <b02901184@ntu.edu.tw>
2021-05-03 17:59:46 +0800

    Close dialogs after getting into preference panel

--- src/web/app/contexts/Dialog-Context.tsx
+++ src/web/app/contexts/Dialog-Context.tsx
@@ -1,45 +1,61 @@
 const React = requireNode('react');
+const PropTypes = requireNode('prop-types');
 const { createContext } = React;
 export const DialogContext = createContext();
 
 export class DialogContextProvider extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            dialogComponents: []
-        }
-    }
+  constructor() {
+    super();
+    this.dialogComponents = [];
+  }
 
-    addDialogComponent = (id, dialogComponent) => {
-        this.state.dialogComponents.push({id, component: dialogComponent});
-        this.setState(this.state);
-    }
+  addDialogComponent = (id: string, dialogComponent: Element): void => {
+    this.dialogComponents.push({ id, component: dialogComponent });
+    this.forceUpdate();
+  };
 
-    isIdExist = (id) => {
-        return this.state.dialogComponents.some((dialog) => dialog.id === id);
-    }
+  isIdExist = (id: string): boolean => this.dialogComponents.some((dialog) => dialog.id === id);
 
-    popDialogById = (id) => {
-        this.state.dialogComponents = this.state.dialogComponents.filter((dialog) => {return dialog.id !== id});
-        this.setState(this.state);
-    }
+  popDialogById = (id: string): void => {
+    this.dialogComponents = this.dialogComponents.filter((dialog) => dialog.id !== id);
+    this.forceUpdate();
+  };
 
-    render() {
-        const { dialogComponents } = this.state;
-        const {
-            addDialogComponent,
-            isIdExist,
-            popDialogById
-        } = this;
-        return (
-            <DialogContext.Provider value={{
-                dialogComponents,
-                addDialogComponent,
-                isIdExist,
-                popDialogById
-            }}>
-                {this.props.children}
-            </DialogContext.Provider>
-        );
-    }
+  clearAllDialogComponents = (): void => {
+    this.dialogComponents = [];
+    this.forceUpdate();
+  };
+
+  render(): Element {
+    const { children } = this.props;
+    const {
+      dialogComponents,
+      addDialogComponent,
+      clearAllDialogComponents,
+      isIdExist,
+      popDialogById,
+    } = this;
+
+    return (
+      <DialogContext.Provider
+        value={{
+          dialogComponents,
+          addDialogComponent,
+          clearAllDialogComponents,
+          isIdExist,
+          popDialogById,
+        }}
+      >
+        {children}
+      </DialogContext.Provider>
+    );
+  }
+}
+
+DialogContextProvider.propTypes = {
+  children: PropTypes.arrayOf(PropTypes.element),
+};
+
+DialogContextProvider.defaultProps = {
+  children: [],
 };
