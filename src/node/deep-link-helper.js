887f86d15555ae2ebf31847ccabe1b20f8f6c076
Dean Sung <a71124725@gmail.com>
2021-05-11 17:07:52 +0800

    Fix deep-link-helper when url is undefined

--- src/node/deep-link-helper.js
+++ src/node/deep-link-helper.js
@@ -1,18 +1,20 @@
 const getDeeplinkUrl = (argv) => {
-    return argv.find((s) => s.startsWith('beam-studio://'));
+  return argv.find((s) => s.startsWith('beam-studio://'));
 };
 
 const handleDeepLinkUrl = (mainWindow, url) => {
+  if (url) {
     url = new URL(decodeURI(url));
     console.log(url)
     if (url.hostname === 'fb-auth') {
-        mainWindow.webContents.send('FB_AUTH_TOKEN', url.hash.slice(1));
+      mainWindow.webContents.send('FB_AUTH_TOKEN', url.hash.slice(1));
     } else if (url.hostname === 'google-auth') {
-        mainWindow.webContents.send('GOOGLE_AUTH', url.search.slice(1));
+      mainWindow.webContents.send('GOOGLE_AUTH', url.search.slice(1));
     }
+  }
 };
 
 module.exports = {
-    getDeeplinkUrl,
-    handleDeepLinkUrl,
+  getDeeplinkUrl,
+  handleDeepLinkUrl,
 }
