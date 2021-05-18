3b8d82c3e6de68d84a254d39103bf8e20d0efb84
Dean Sung <dean@flux3dp.com>
2021-05-18 10:31:33 +0800

    Update workflow files (#87)

---
+++ build/clearup-develop-files.bat
@@ -0,0 +1,6 @@
+Del /S /Q /F curl.exe
+Del /S /Q /F util
+Del /S /Q /F lib
+Del /S /Q /F .github
+Del /S /Q /F .\src\web
+Del /S /Q /F .\public\sass
---
+++ build/clearup-develop-files.sh
@@ -0,0 +1,6 @@
+rm -f curl.exe
+rm -rf util
+rm -rf lib
+rm -rf .github
+rm -rf ./src/web
+rm -rf ./public/sass
