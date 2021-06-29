070ccb772f1adac112a8863bbe34da8fb757f1ae
Yanting Chen <ChenYanting@users.noreply.github.com>
2021-06-29 09:21:42 +0800

    move electron-specific action here (#153)
    * move electron-specific action here
    
    * add missing function

--- src/implementations/menu.ts
+++ src/implementations/menu.ts
@@ -1,9 +1,18 @@
 import AbstractMenu from 'helpers/menubar/AbstractMenu';
 import BeamboxPreference from 'app/actions/beambox/beambox-preference';
 
+import communicator from 'implementations/communicator';
+import ElectronUpdater from '../electron-updater';
 import { updateCheckbox } from '../electron-menubar-helper';
 
 class Menu extends AbstractMenu {
+  private communicator;
+
+  constructor(aCommunicator) {
+    super();
+    this.communicator = aCommunicator;
+  }
+
   init(): void {
     const shouldShowRulers = !!BeamboxPreference.read('show_rulers');
     updateCheckbox(['_view', 'SHOW_RULERS'], shouldShowRulers);
@@ -14,6 +23,23 @@ class Menu extends AbstractMenu {
 
     this.initMenuEvents();
   }
+
+  attach(enabledItems: string[]) {
+    super.attach(enabledItems);
+    ElectronUpdater.autoCheck();
+  }
+
+  enable(items: string[]): void {
+    if (this.communicator) {
+      this.communicator.send('ENABLE_MENU_ITEM', items);
+    }
+  }
+
+  disable(items: string[]): void {
+    if (this.communicator) {
+      this.communicator.send('DISABLE_MENU_ITEM', items);
+    }
+  }
 }
 
-export default new Menu();
+export default new Menu(communicator);
