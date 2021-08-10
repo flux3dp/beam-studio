29861748ac3983566daf732f3d3932fdf3747a28
Dean Sung <a71124725@gmail.com>
2021-08-10 14:24:04 +0800

    Fix eslints

--- src/node/main-window-entry.js
+++ src/node/main-window-entry.js
@@ -1,8 +1,13 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable global-require */
+/* eslint-disable @typescript-eslint/no-var-requires */
 
-(function (G) {
+(function mainEntry(G) {
   const path = require('path');
 
+  // eslint-disable-next-line import/no-extraneous-dependencies
   const { ipcRenderer, remote } = require('electron');
+  // eslint-disable-next-line import/no-dynamic-require
   const events = require(path.join(__dirname, 'ipc-events'));
 
   // For modules not loadable in renderer process
@@ -12,7 +17,7 @@
 
   G.electron = {
     ipc: ipcRenderer,
-    events: events,
+    events,
     remote,
   };
-})(global)
+}(global));
