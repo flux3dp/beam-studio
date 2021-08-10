9d6e3761c7cf19d1f4092b0eb650b55e5d7e63fb
Dean Sung <a71124725@gmail.com>
2021-08-10 13:15:48 +0800

    Update menu open handler

--- src/node/main-window-entry.js
+++ src/node/main-window-entry.js
@@ -2,23 +2,12 @@
 (function(G) {
     const path = require('path');
 
-    const {ipcRenderer, webFrame, remote} = require('electron');
+    const { ipcRenderer, remote } = require('electron');
     const events = require(path.join(__dirname, 'ipc-events'));
-
-    // For modules not loadable in renderer process
-    G.nodeModules = {
-        '@sentry/electron': require('@sentry/electron'),
-    }
 
     G.electron = {
         ipc: ipcRenderer,
         events: events,
-        trigger_file_input_click: (inputId) => {
-            if(inputId.match(/^[a-zA-Z0-9\-\_]+$/)) {
-                webFrame.executeJavaScript(
-                    `document.querySelector("[data-file-input=${inputId}]").click()`, true);
-            }
-        },
         remote,
     }
 })(global)
