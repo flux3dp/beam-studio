486516774e5e797f1cc13560fc8f19b76a9f9ce2
Dean Sung <a71124725@gmail.com>
2021-08-10 14:11:16 +0800

    Update indent

--- src/node/main-window-entry.js
+++ src/node/main-window-entry.js
@@ -1,18 +1,18 @@
 
-(function(G) {
-    const path = require('path');
+(function (G) {
+  const path = require('path');
 
-    const { ipcRenderer, remote } = require('electron');
-    const events = require(path.join(__dirname, 'ipc-events'));
+  const { ipcRenderer, remote } = require('electron');
+  const events = require(path.join(__dirname, 'ipc-events'));
 
-    // For modules not loadable in renderer process
-    G.nodeModules = {
-        '@sentry/electron': require('@sentry/electron'),
-    }
+  // For modules not loadable in renderer process
+  G.nodeModules = {
+    '@sentry/electron': require('@sentry/electron'),
+  }
 
-    G.electron = {
-        ipc: ipcRenderer,
-        events: events,
-        remote,
-    }
+  G.electron = {
+    ipc: ipcRenderer,
+    events: events,
+    remote,
+  }
 })(global)
