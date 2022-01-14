6f9533fe8ee65defa4006c79bffdd4371ee09cc1
Dean Sung <a71124725@gmail.com>
2022-01-14 10:42:46 +0800

    Update change log

--- src/implementations/changelog.ts
+++ src/implementations/changelog.ts
@@ -2,31 +2,27 @@ import { IChangeLog } from 'interfaces/IChangeLog';
 
 const CHANGES_TW = {
   added: [
-    '新增多邊形物件邊數設定的選項。',
-    '關於 Bema Studio 面板新增開放原始碼資訊說明。',
+    '新增 HEXA 工作範圍以及雷射參數。',
   ],
   fixed: [
-    '修正某些 DXF 物件路徑計算錯誤的問題。',
-    '修正讀取部分 SVG 檔顯示錯誤。',
-    '雷射參數列表新增參數圖示更正。',
+    '修正工作預覽在圖層速度較快時，預估時間會和計算工作時間不同的問題。',
+    '修正無法讀取資料夾路徑中含有 # 字號的檔案。',
+    '修正文字在旋轉並縮放之後移動會導致位置計算錯誤的問題。',
   ],
   changed: [
-    '文字縮放預設鎖定長寬比例。',
   ],
 };
 
 const CHANGES_EN = {
   added: [
-    'Added the Sides option for polygon objects.',
-    'Added the information for the open-source software in the About Beam Studio.',
+    'Added workarea and laser presets of HEXA.',
   ],
   fixed: [
-    'Fixed some DXF file calculate path failed problems.',
-    'Fixed some SVG files wrong display problems.',
-    'Fixed the added icon in the Preset Manage panel.',
+    'Fixed time estimation deviation in path preview mode when engraving speed is fast.',
+    'Fixed failed to load files whose path containing #.',
+    'Fixed position error when moving rotated and scaled text elements.',
   ],
   changed: [
-    'Freeze the scale ratio for the font objects by default.',
   ],
 };
 
