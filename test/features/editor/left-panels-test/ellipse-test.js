78a5c7339d48f61b67a3f713caf957fd5bc3d2b9
Jess <68686011+Jess-Jian@users.noreply.github.com>
2022-02-08 13:33:22 +0800

    Feature/spectron-left-panels (1/2) (#200)
    * remove about jest-electron files
    
    * rework ellipse.test.js
    
    * rework line.test.js
    
    * change variable name of ellipse file with camel case
    
    * change variable name of line file with camel case
    
    * rework pen.test.js
    
    * rework pen.test.js again
    
    * rework polygon.test.js
    
    * rework editor.js, action.js and utils.js
    
    * update version
    
    * uncomment out
    
    * use const to replace let

--- test/features/editor/left-panels-test/ellipse-test.js
+++ test/features/editor/left-panels-test/ellipse-test.js
@@ -2,42 +2,44 @@ const { checkExist, setReload } = require('../../../util/utils');
 const { pageCoordtoCanvasCoord } = require('../../../util/editor-utils');
 const { mouseAction } = require('../../../util/actions');
 
-test('Check Draw Ellipse', async function() {
-    let { app } = require('../../../test');
-    await setReload();
-    await checkExist('#svgcanvas', 15000);
+test('Check Draw Ellipse', async function () {
+  let { app } = require('../../../test');
+  await setReload();
+  await checkExist('#svgcanvas', 15000);
 
-    const elli = await app.client.$('#left-Ellipse');
-    await elli.click();
+  const elli = await app.client.$('#left-Ellipse');
+  await elli.click();
 
-    await mouseAction([
-        { type: 'pointerMove', x: 400, y: 400, duration: 100, },
-        { type: 'pointerDown', button: 0, },
-        { type: 'pointerMove', x: 464, y: 464, duration: 1000, },
-        { type: 'pointerUp', button: 0, },
-    ]);
-    await checkExist('#svg_1');
+  await mouseAction([
+    { type: 'pointerMove', x: 400, y: 400, duration: 100, },
+    { type: 'pointerDown', button: 0, },
+    { type: 'pointerMove', x: 464, y: 464, duration: 1000, },
+    { type: 'pointerUp', button: 0, },
+  ]);
+  await checkExist('#svg_1');
 
-    const startPoint = await pageCoordtoCanvasCoord({x: 400, y: 400});
-    const endPoint = await pageCoordtoCanvasCoord({x: 464, y: 464});
-    let expectedX = startPoint.x;
-    let expectedY = startPoint.y;
-    let expectedW = Math.pow(endPoint.x - startPoint.x, 2) ;
-    let expectedH = Math.pow(endPoint.y - startPoint.y, 2) ;
-    let expectedR = Math.sqrt(expectedW + expectedH)/Math.sqrt(2);
-    //console.log(expectedR);
-    const svg_1cx = await app.client.$('#svg_1');
-    const actualCX = await svg_1cx.getAttribute('cx');
+  const startPoint = await pageCoordtoCanvasCoord({ x: 400, y: 400 });
+  const endPoint = await pageCoordtoCanvasCoord({ x: 464, y: 464 });
+  let expectedX = startPoint.x;
+  let expectedY = startPoint.y;
+  let expectedW = Math.pow(endPoint.x - startPoint.x, 2);
+  let expectedH = Math.pow(endPoint.y - startPoint.y, 2);
+  let expectedR = Math.sqrt(expectedW + expectedH) / Math.sqrt(2);
 
-    const svg_1cy = await app.client.$('#svg_1');
-    const actualCY = await svg_1cy.getAttribute('cy');
+  const svgCx = await app.client.$('#svg_1');
+  const actualCx = await svgCx.getAttribute('cx');
 
-    const svg_1rx = await app.client.$('#svg_1');
-    const actualR = await svg_1rx.getAttribute('rx');
-    
-    const aCX = parseFloat(actualCX).toFixed(10);
-    const aCY = parseFloat(actualCY).toFixed(10);
-    const aR = parseFloat(actualR).toFixed(10);
-    expect(Math.abs(expectedX - aCX)).toBeLessThanOrEqual(1e-10);
-    expect(Math.abs(expectedY - aCY)).toBeLessThanOrEqual(1e-10);
+  const svgCy = await app.client.$('#svg_1');
+  const actualCy = await svgCy.getAttribute('cy');
+
+  const svgRx = await app.client.$('#svg_1');
+  const actualR = await svgRx.getAttribute('rx');
+
+  const cx = parseFloat(actualCx).toFixed(10);
+  const cy = parseFloat(actualCy).toFixed(10);
+  const rx = parseFloat(actualR).toFixed(10);
+
+  expect(Math.abs(expectedX - cx)).toBeLessThanOrEqual(1e-10);
+  expect(Math.abs(expectedY - cy)).toBeLessThanOrEqual(1e-10);
+  expect(Math.abs(expectedR - rx)).toBeLessThanOrEqual(1e-10);
 });
