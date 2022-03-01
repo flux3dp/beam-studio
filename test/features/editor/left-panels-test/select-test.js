6fb31a9e9f1afa78f01f87d4102c1656d5271a50
Jess <jess@flux3dp.com>
2022-03-01 15:51:21 +0800

    rework select-test.js

--- test/features/editor/left-panels-test/select-test.js
+++ test/features/editor/left-panels-test/select-test.js
@@ -1,85 +1,84 @@
 const { checkExist, setReload } = require('../../../util/utils');
 const { mouseAction } = require('../../../util/actions');
 
-test('Check Select', async function() {
-    const { app } = require('../../../test');
-    await setReload();
-    await checkExist('#svgcanvas',15000);
+describe('Verify Select Tool', () => {
+  beforeEach(() => {
+    setReload();
+    checkExist('#svgcanvas', 15000);
+  });
 
+  test('Check Select', async function () {
+    const { app } = require('../../../test');
     const polygon = await app.client.$('#left-Polygon');
     await polygon.click();
 
     await mouseAction([
-        { type: 'pointerMove', x: 200, y: 200, duration: 100, },
-        { type: 'pointerDown', button: 0, },
-        { type: 'pointerMove', x: 300, y: 300, duration: 1000, },
-        { type: 'pointerUp', button: 0, },
+      { type: 'pointerMove', x: 200, y: 200, duration: 100, },
+      { type: 'pointerDown', button: 0, },
+      { type: 'pointerMove', x: 300, y: 300, duration: 1000, },
+      { type: 'pointerUp', button: 0, },
     ]);
-    
+
     await mouseAction([
-        { type: 'pointerMove', x: 200, y: 200, duration: 100, },
-        { type: 'pointerDown', button: 0, },
-        { type: 'pointerMove', x: 500, y: 500, duration: 1000, },
-        { type: 'pointerUp', button: 0, },
+      { type: 'pointerMove', x: 200, y: 200, duration: 100, },
+      { type: 'pointerDown', button: 0, },
+      { type: 'pointerMove', x: 500, y: 500, duration: 1000, },
+      { type: 'pointerUp', button: 0, },
     ]);
     await new Promise((r) => setTimeout(r, 1000));
-    
-    const result = await app.client.execute(() =>{
-        return svgCanvas.getSelectedElems();
+
+    const result = await app.client.execute(() => {
+      return svgCanvas.getSelectedElems();
     });
     expect(result.length).toEqual(1);
 
     const id = await app.client.execute(() => {
-        const e = svgCanvas.getSelectedElems();
-        return e.map((e) => {return e.getAttribute('id')});
+      const e = svgCanvas.getSelectedElems();
+      return e.map((e) => { return e.getAttribute('id') });
     });
     expect(id).toEqual(["svg_1"]);
+  });
 
-});
-
-test('Check Multi-Select', async function() {
+  test('Check Multi-Select', async function () {
     const { app } = require('../../../test');
-    
-    await setReload();
-    await checkExist('#svgcanvas',15000);
-
     const rect = await app.client.$('#left-Rectangle');
     await rect.click();
 
     await mouseAction([
-        { type: 'pointerMove', x: 200, y: 200, duration: 100, },
-        { type: 'pointerDown', button: 0, },
-        { type: 'pointerMove', x: 300, y: 300, duration: 1000, },
-        { type: 'pointerUp', button: 0, },
+      { type: 'pointerMove', x: 200, y: 200, duration: 100, },
+      { type: 'pointerDown', button: 0, },
+      { type: 'pointerMove', x: 300, y: 300, duration: 1000, },
+      { type: 'pointerUp', button: 0, },
     ]);
 
     const elli = await app.client.$('#left-Ellipse');
     await elli.click();
 
     await mouseAction([
-        { type: 'pointerMove', x: 400, y: 400, duration: 100, },
-        { type: 'pointerDown', button: 0, },
-        { type: 'pointerMove', x: 450, y: 450, duration: 1000, },
-        { type: 'pointerUp', button: 0, },
+      { type: 'pointerMove', x: 400, y: 400, duration: 100, },
+      { type: 'pointerDown', button: 0, },
+      { type: 'pointerMove', x: 450, y: 450, duration: 1000, },
+      { type: 'pointerUp', button: 0, },
     ]);
 
     await mouseAction([
-        { type: 'pointerMove', x: 150, y: 150, duration: 100, },
-        { type: 'pointerDown', button: 0, },
-        { type: 'pointerMove', x: 500, y: 500, duration: 1000, },
-        { type: 'pointerUp', button: 0, },
+      { type: 'pointerMove', x: 150, y: 150, duration: 100, },
+      { type: 'pointerDown', button: 0, },
+      { type: 'pointerMove', x: 500, y: 500, duration: 1000, },
+      { type: 'pointerUp', button: 0, },
     ]);
     await new Promise((r) => setTimeout(r, 1000));
 
-    const result = await app.client.execute(() =>{
-        let g = svgCanvas.getTempGroup();
-        let childNodes = Array.from(g.childNodes);
-        const rectangle = document.getElementById('svg_1');
-        const ellipse = document.getElementById('svg_2');
-        const isRectInsideGroup =  childNodes.includes(rectangle);
-        const isEllipseInsideGroup =  childNodes.includes(ellipse);
-        return {isRectInsideGroup, isEllipseInsideGroup};
+    const result = await app.client.execute(() => {
+      let g = svgCanvas.getTempGroup();
+      let childNodes = Array.from(g.childNodes);
+      const rectangle = document.getElementById('svg_1');
+      const ellipse = document.getElementById('svg_2');
+      const isRectInsideGroup = childNodes.includes(rectangle);
+      const isEllipseInsideGroup = childNodes.includes(ellipse);
+      return { isRectInsideGroup, isEllipseInsideGroup };
     });
     expect(result.isRectInsideGroup).toBe(true);
     expect(result.isEllipseInsideGroup).toBe(true);
+  });
 });
