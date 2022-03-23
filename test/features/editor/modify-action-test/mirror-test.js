65ed25e9c174361dbf1a34601cf4e982cf28c65b
Jess <jess@flux3dp.com>
2022-03-23 14:48:56 +0800

    rework mirror.test.js

--- test/features/editor/modify-action-test/mirror-test.js
+++ test/features/editor/modify-action-test/mirror-test.js
@@ -1,48 +1,41 @@
 const { checkExist, setReload } = require('../../../util/utils');
 const { mouseAction } = require('../../../util/actions');
 
-test('Check Horizontal Flip', async function() {
-    const { app } = require('../../../test');
-    await setReload();
-    await checkExist('#svgcanvas',15000);
+describe('Verify Mirror Tool', () => {
+  beforeEach(() => {
+    setReload();
+    checkExist('#svgcanvas', 15000);
+  });
 
+  test('Check Horizontal Flip', async function () {
+    const { app } = require('../../../test');
+    await typing();
+    const horizontalBtn = await app.client.$('div#horizontal_flip.tool-btn');
+    await horizontalBtn.click();
+    const svg = await app.client.$('#svg_1');
+    const str = await svg.getAttribute('transform');
+    expect(str.substring(7, 15)).toEqual('-1,0,0,1')
+  });
+
+  test('Check Vertical Flip', async function () {
+    const { app } = require('../../../test');
+    await typing();
+    const verticalBtn = await app.client.$('div#vertical_flip.tool-btn');
+    await verticalBtn.click();
+    const svg = await app.client.$('#svg_1');
+    const str = await svg.getAttribute('transform');
+    expect(str.substring(7, 15)).toEqual('1,0,0,-1');
+  });
+
+  async function typing() {
+    const { app } = require('../../../test');
     const text = await app.client.$('#left-Text');
     await text.click();
-  
     await mouseAction([
-        { type: 'pointerMove', x: 300, y: 300, duration: 10, },
-        { type: 'pointerDown', button: 0, },
-        { type: 'pointerUp', button: 0, },
+      { type: 'pointerMove', x: 300, y: 300, duration: 10, },
+      { type: 'pointerDown', button: 0, },
+      { type: 'pointerUp', button: 0, },
     ]);
     await app.client.keys(['MIRROR', 'Space', 'TEST']);
-
-    const HorizontalFlip = await app.client.$('div#horizontal_flip.tool-btn');
-    await HorizontalFlip.click();
-    const textnotransform = await app.client.$('#svg_1');
-    const transformvalue = await textnotransform.getAttribute('transform');
-    // console.log(transformvalue);
-    expect(transformvalue).toEqual('matrix(-1,0,0,1,1662.203125,0) ');
-    await new Promise((r) => setTimeout(r, 1000));
-
-    const HorizontalFlip_1 = await app.client.$('div#horizontal_flip.tool-btn');
-    await HorizontalFlip_1.click();
-    const notransformvalue = await textnotransform.getAttribute('transform');
-    expect(notransformvalue).toEqual('matrix(1,0,0,1,0,0) ');
-});
-
-test('Check Vertical Flip', async function() {
-    const { app } = require('../../../test');
-
-    const VerticalFlip = await app.client.$('div#vertical_flip.tool-btn');
-    await VerticalFlip.click();
-    const texttransform = await app.client.$('#svg_1');
-    const transformvalue = await texttransform.getAttribute('transform');
-    // console.log(transformvalue);
-    expect(transformvalue).toEqual('matrix(1,0,0,-1,0,1483.3125) ');
-    await new Promise((r) => setTimeout(r, 1000));
-
-    const VerticalFlip_1 = await app.client.$('div#vertical_flip.tool-btn');
-    await VerticalFlip_1.click();
-    const notransformvalue = await texttransform.getAttribute('transform');
-    expect(notransformvalue).toEqual('matrix(1,0,0,1,0,0) ');
+  };
 });
