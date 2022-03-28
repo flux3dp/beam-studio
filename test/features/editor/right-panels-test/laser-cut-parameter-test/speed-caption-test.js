edf07c12a8ce4884c5ef26569d2e30f5ffed898d
Jess <jess@flux3dp.com>
2022-03-28 13:44:33 +0800

    rework speed-caption.test.js

--- test/features/editor/right-panels-test/laser-cut-parameter-test/speed-caption-test.js
+++ test/features/editor/right-panels-test/laser-cut-parameter-test/speed-caption-test.js
@@ -8,31 +8,24 @@ test('Check Speed Caption', async function() {
 
     const rect = await app.client.$('#left-Rectangle');
     await rect.click();
-
     await mouseAction([
         { type: 'pointerMove', x: 300, y: 300, duration: 100, },
         { type: 'pointerDown', button: 0, },
         { type: 'pointerMove', x: 500, y: 500, duration: 1000, },
         { type: 'pointerUp', button: 0, },
     ]);
+    const tabButton = await app.client.$('div.tab.layers');
+    await tabButton.click();
 
-    const layerbutton = await app.client.$('div.tab.layers');
-    await layerbutton.click(); 
-
-    const speed = await app.client.$('input#speed');
-    await speed.doubleClick();
+    const speedInput = await app.client.$('input#speed');
+    await speedInput.doubleClick();
     await app.client.keys(['Backspace', '1', '2', '0', 'Enter',"NULL"]);
+    expect(await speedInput.getAttribute('value')).toEqual('120');
 
-    const input = await app.client.$('input#speed' );
-    const strengthvalue =await input.getAttribute('value');
-    expect(strengthvalue).toEqual('120');
-    
-    const gobutton = await app.client.$('div.go-button-container');
-    await gobutton.click(); 
-
+    const goButton = await app.client.$('div.go-button-container');
+    await goButton.click();
     await new Promise((r) => setTimeout(r, 1000));
     await checkExist('div.modal-alert.animate__animated.animate__bounceIn',1500);
-
     const next = await app.client.$('button.btn.btn-default.primary');
-    await next.click();    
+    await next.click();
 });
