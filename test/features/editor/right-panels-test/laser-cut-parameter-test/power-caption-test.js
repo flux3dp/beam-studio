9431bf40d1234b83ddc77b877bb5ad9612733ab4
Jess <jess@flux3dp.com>
2022-03-28 13:21:03 +0800

    rework power-caption.test.js

--- test/features/editor/right-panels-test/laser-cut-parameter-test/power-caption-test.js
+++ test/features/editor/right-panels-test/laser-cut-parameter-test/power-caption-test.js
@@ -1,25 +1,22 @@
 const { checkExist, setReload } = require('../../../../util/utils');
 
-test('Change Power Caption', async function() {
-    const { app } = require('../../../../test');
-    await setReload();
-    await checkExist('#svgcanvas',15000);
-    const power = await app.client.$('input#power');
-    await power.doubleClick();
-    await app.client.keys(['Backspace', '7', '5', 'Enter',"NULL"]);
+test('Change Power Caption', async function () {
+  const { app } = require('../../../../test');
+  await setReload();
+  await checkExist('#svgcanvas', 15000);
+  const powerInput = await app.client.$('input#power');
+  await powerInput.doubleClick();
+  await app.client.keys(['Backspace', '7', '5', 'Enter', "NULL"]);
 
-    const input = await app.client.$('input#power' );
-    const strengthvalue =await input.getAttribute('value');
-    expect(strengthvalue).toEqual('75');
-    await new Promise((r) => setTimeout(r, 1000));
+  expect(await powerInput.getAttribute('value')).toEqual('75');
+  await new Promise((r) => setTimeout(r, 1000));
 
-    const gobutton = await app.client.$('div.go-button-container');
-    await gobutton.click(); 
+  const goButton = await app.client.$('div.go-button-container');
+  await goButton.click();
 
-    await new Promise((r) => setTimeout(r, 1000));
-    await checkExist('div#qa-power-caption',15000);
+  await new Promise((r) => setTimeout(r, 1000));
+  await checkExist('div#power-caption', 15000);
 
-    const next = await app.client.$('button.btn.btn-default.primary');
-    await next.click();    
-
+  const next = await app.client.$('button.btn.btn-default.primary');
+  await next.click();
 });
