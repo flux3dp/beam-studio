859372d38fb34371fb8ad710c3dfb8c147670377
Dean Sung <a71124725@gmail.com>
2022-04-29 10:13:22 +0800

    Update change log for 1.9.3

--- src/implementations/changelog.ts
+++ src/implementations/changelog.ts
@@ -2,33 +2,29 @@ import { IChangeLog } from 'interfaces/IChangeLog';
 
 const CHANGES_TW = {
   added: [
-    '新增 HEXA 工作範圍以及雷射參數。',
   ],
   fixed: [
-    '修正工作預覽在圖層速度較快時，預估時間會和計算工作時間不同的問題。',
-    '修正無法讀取資料夾路徑中含有 # 字號的檔案。',
-    '修正文字在旋轉並縮放之後移動會導致位置計算錯誤的問題。',
-    '修正 Windows 系統檔案匯入問題',
-    '修正圖層色彩面板顯示問題',
-    '修正部分 Windows 系統讀取 PDF, AI 檔時的問題',
+    '修正在圖層管理面板修改參數後，雷射面板選單參數不會立即套用。',
+    '修正部分德文翻譯。',
+    '修正部分檔案多次點擊後位移問題。',
   ],
   changed: [
+    '將「使用圖層顏色」預設值改為開。',
+    '更改新手教學對焦連結。',
   ],
 };
 
 const CHANGES_EN = {
   added: [
-    'Added workarea and laser presets of HEXA.',
   ],
   fixed: [
-    'Fixed time estimation deviation in path preview mode when engraving speed is fast.',
-    'Fixed failed to load files whose path containing #.',
-    'Fixed position error when moving rotated and scaled text elements.',
-    'Fixed file import bug in Windows.',
-    'Fixed layer color picker panel display bug.',
-    'Fixed AI, PDF files loading issue for certain Windows computers.',
+    'Fixed laser panel presets were not applied after editing presets in preset management panel.',
+    'Fixed some Deutsch translations',
+    'Fixed position shifting error after repeating selecting some objects in some files.',
   ],
   changed: [
+    'Changed "Use Layer Color" default value to on.',
+    'Changed "How to focus" link in tutorial.',
   ],
 };
 
