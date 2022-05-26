1f9beb7c22ca5e47a692bb408dc0f60bb497a470
Dean Sung <a71124725@gmail.com>
2022-05-26 18:31:55 +0800

    Update package.json and changelog

--- src/implementations/changelog.ts
+++ src/implementations/changelog.ts
@@ -4,13 +4,9 @@ const CHANGES_TW = {
   added: [
   ],
   fixed: [
-    '修正在圖層管理面板修改參數後，雷射面板選單參數不會立即套用。',
-    '修正部分德文翻譯。',
-    '修正部分檔案多次點擊後位移問題。',
+    '修正旋轉後縮放後位移問題。',
   ],
   changed: [
-    '將「使用圖層顏色」預設值改為開。',
-    '更改新手教學對焦連結。',
   ],
 };
 
@@ -18,13 +14,9 @@ const CHANGES_EN = {
   added: [
   ],
   fixed: [
-    'Fixed laser panel presets were not applied after editing presets in preset management panel.',
-    'Fixed some Deutsch translations',
-    'Fixed position shifting error after repeating selecting some objects in some files.',
+    'Fixed position shifting error after rotating and scaling some objects in some files.',
   ],
   changed: [
-    'Changed "Use Layer Color" default value to on.',
-    'Changed "How to focus" link in tutorial.',
   ],
 };
 
