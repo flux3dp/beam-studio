4c6f0c7bea02c2c486c9797c81290ced563ff01d
Dean Sung <a71124725@gmail.com>
2022-07-05 14:18:24 +0800

    Update changelog

--- src/implementations/changelog.ts
+++ src/implementations/changelog.ts
@@ -2,31 +2,23 @@ import { IChangeLog } from 'interfaces/IChangeLog';
 
 const CHANGES_TW = {
   added: [
-    '新增多邊形物件邊數設定的選項。',
-    '關於 Bema Studio 面板新增開放原始碼資訊說明。',
   ],
   fixed: [
-    '修正某些 DXF 物件路徑計算錯誤的問題。',
-    '修正讀取部分 SVG 檔顯示錯誤。',
-    '雷射參數列表新增參數圖示更正。',
+    '修正依顏色讀取 svg 檔時顏色與原始有些為出入。',
   ],
   changed: [
-    '文字縮放預設鎖定長寬比例。',
+    '變更一些日文翻譯。',
   ],
 };
 
 const CHANGES_EN = {
   added: [
-    'Added the Sides option for polygon objects.',
-    'Added the information for the open-source software in the About Beam Studio.',
   ],
   fixed: [
-    'Fixed some DXF file calculate path failed problems.',
-    'Fixed some SVG files wrong display problems.',
-    'Fixed the added icon in the Preset Manage panel.',
+    'Fixed minor color change when loading file divded layer by color.',
   ],
   changed: [
-    'Freeze the scale ratio for the font objects by default.',
+    'Changed some Japanese translations.',
   ],
 };
 
