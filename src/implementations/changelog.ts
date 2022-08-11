04d79a96dea1b7a0f4e18b0e0af998190e9f4224
Dean Sung <a71124725@gmail.com>
2022-08-11 12:07:00 +0800

    Update changelog

--- src/implementations/changelog.ts
+++ src/implementations/changelog.ts
@@ -5,6 +5,7 @@ const CHANGES_TW = {
   ],
   fixed: [
     '修正部分圖檔無法輸出工作問題',
+    '修正工作預覽中途開始功能的多餘 FCode 行數',
   ],
   changed: [
     '加高圖層面板寬度。',
@@ -16,6 +17,7 @@ const CHANGES_EN = {
   ],
   fixed: [
     'Fixed some files cannot export task.',
+    'Fixed redundant lines in FCode of start here button in path preview.',
   ],
   changed: [
     'Change the height of layer list panel.',
