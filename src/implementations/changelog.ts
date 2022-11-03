6e669397b851b7354d92d531a52e8f72db9ff389
Dean Sung <a71124725@gmail.com>
2022-11-03 11:28:14 +0800

    1.9.11 beta

--- src/implementations/changelog.ts
+++ src/implementations/changelog.ts
@@ -7,7 +7,7 @@ const CHANGES_TW = {
     '修正新手教學時提示框錯位。',
     '修正相機預覽發生錯誤時顯示方式。',
     '修正文字旋轉後編輯錯位問題。',
-    '修正匯入的 SVG 檔案複製後導致圖片無法讀取問題。',
+    '修正匯入的 SVG 檔案複製後無法顯示問題。',
   ],
   changed: [
     '調整 HEXA 雕刻參數',
@@ -21,7 +21,7 @@ const CHANGES_EN = {
     'Fixed tutorial hint box positioning issues.',
     'Fixed error message of camera calibration.',
     'Fixed text positioning issue after rotating and editing.',
-    'Fixed image loading issue after copying and pasting imported SVG objects.',
+    'Fixed image displaying issue after copying and pasting imported SVG objects.',
   ],
   changed: [
     'Updated HEXA engraving presets.',
