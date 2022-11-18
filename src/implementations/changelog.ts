f6a6835eb77447d63cf3910a879bfce17422b7ce
Dean Sung <dean@flux3dp.com>
2022-11-18 09:56:02 +0800

    1.9.11 beta

--- src/implementations/changelog.ts
+++ src/implementations/changelog.ts
@@ -2,29 +2,25 @@ import { IChangeLog } from 'interfaces/IChangeLog';
 
 const CHANGES_TW = {
   added: [
+    '新增法文',
+    '新增荷蘭文',
   ],
   fixed: [
-    '修正新手教學時提示框錯位。',
-    '修正相機預覽發生錯誤時顯示方式。',
-    '修正文字旋轉後編輯錯位問題。',
     '修正匯入的 SVG 檔案複製後無法顯示問題。',
   ],
   changed: [
-    '調整 HEXA 雕刻參數',
   ],
 };
 
 const CHANGES_EN = {
   added: [
+    'Added Français',
+    'Added Nederlands',
   ],
   fixed: [
-    'Fixed tutorial hint box positioning issues.',
-    'Fixed error message of camera calibration.',
-    'Fixed text positioning issue after rotating and editing.',
     'Fixed image displaying issue after copying and pasting imported SVG objects.',
   ],
   changed: [
-    'Updated HEXA engraving presets.',
   ],
 };
 
