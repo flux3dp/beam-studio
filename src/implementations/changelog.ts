60c18407a91574cebc40fdfc9b98f57be7634ca2
Dean Sung <dean@flux3dp.com>
2023-09-06 14:21:14 +0800

    2.1.1 alpha

--- src/implementations/changelog.ts
+++ src/implementations/changelog.ts
@@ -1,27 +1,36 @@
 import { IChangeLog } from 'interfaces/IChangeLog';
 
-// 2.1.0-beta
+// 2.1.1-beta
 const CHANGES_TW = {
   added: [
-    '新增圖片 AI 去背功能（需登入後使用）。',
     '新增圖片勾勒輪廓功能。',
+    '新增合併文字功能。',
   ],
   fixed: [
-    '修正 Windows 工具列機器列表需重新整理才會更新。',
+    '修正對矩形進行布林操作時圓角會消失。',
+    '修正調整文件設定模組後，圖層面板不會更新。',
+    '修正直書文字選項沒有標題。',
   ],
   changed: [
+    '更改讀取對話框形式。',
+    '移除標題工具列的文字底線。',
   ],
 };
 
 const CHANGES_EN = {
   added: [
-    'Added AI background removal function for image (need to login to use).',
     'Added image outline action.',
+    'Added weld text action.',
   ],
   fixed: [
-    'Fixed Windows toolbar machine list need to refresh to update.',
+    'Fixed the rounded corner of the rectangle will disappear when boolean operation.',
+    'Fixed the layer panel will not update after changing addons in the document settings.',
+    'Fixed the vertical text option has no title.',
   ],
   changed: [
+    'Changed the loading dialog.',
+    'Changed some Japanese translations.',
+    'Removed the underscores of the titlebar.',
   ],
 };
 
