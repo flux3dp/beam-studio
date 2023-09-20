4564e399fcb6af63a134480693ad769e009f3a0c
Dean Sung <dean@flux3dp.com>
2023-09-20 10:12:15 +0800

    Change version to 2.1.2

--- src/implementations/changelog.ts
+++ src/implementations/changelog.ts
@@ -1,36 +1,31 @@
 import { IChangeLog } from 'interfaces/IChangeLog';
 
-// 2.1.1-beta
+// 2.1.2-beta
 const CHANGES_TW = {
   added: [
-    '新增圖片勾勒輪廓功能。',
-    '新增合併文字功能。',
+    '新增 MacOS 版本提示。',
   ],
   fixed: [
-    '修正對矩形進行布林操作時圓角會消失。',
-    '修正調整文件設定模組後，圖層面板不會更新。',
-    '修正直書文字選項沒有標題。',
+    '修正相機預覽時第一次點擊畫面沒有進行預覽。',
+    '修正在畫面最左方進行相機預覽時，會有預覽誤差。',
+    '修正雕刻檔案時間變長問題。',
+    '修正合併文字路徑問題。',
   ],
   changed: [
-    '更改讀取對話框形式。',
-    '移除標題工具列的文字底線。',
   ],
 };
 
 const CHANGES_EN = {
   added: [
-    'Added image outline action.',
-    'Added weld text action.',
+    'Added the MacOS version prompt.',
   ],
   fixed: [
-    'Fixed the rounded corner of the rectangle will disappear when boolean operation.',
-    'Fixed the layer panel will not update after changing addons in the document settings.',
-    'Fixed the vertical text option has no title.',
+    'Fixed the preview error when previewing the first time.',
+    'Fixed the preview error when previewing the left side of the screen.',
+    'Fixed the engraving file time issue.',
+    'Fixed the merge text path issue.',
   ],
   changed: [
-    'Changed the loading dialog.',
-    'Changed some Japanese translations.',
-    'Removed the underscores of the titlebar.',
   ],
 };
 
