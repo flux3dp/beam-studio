66c8cd79935cf118a4f8e1921eea313943d3b660
Dean Sung <dean@flux3dp.com>
2023-10-03 16:51:45 +0800

    Change to 2.1.3

--- src/implementations/changelog.ts
+++ src/implementations/changelog.ts
@@ -1,15 +1,14 @@
 import { IChangeLog } from 'interfaces/IChangeLog';
 
-// 2.1.2-beta
+// 2.1.3-beta
 const CHANGES_TW = {
   added: [
-    '新增 MacOS 版本提示。',
+    '新增「合併文字」功能 Icon。',
+    '新增左側形狀面板',
+    '新增視窗響應式設計。',
   ],
   fixed: [
-    '修正相機預覽時第一次點擊畫面沒有進行預覽。',
-    '修正在畫面最左方進行相機預覽時，會有預覽誤差。',
-    '修正雕刻檔案時間變長問題。',
-    '修正合併文字路徑問題。',
+    '修正開啟開蓋模式時高解析度下工作範圍錯誤。',
   ],
   changed: [
   ],
@@ -17,13 +16,12 @@ const CHANGES_TW = {
 
 const CHANGES_EN = {
   added: [
-    'Added the MacOS version prompt.',
+    'Added icon for "Weld Text".',
+    'Added shape panel in left panel.',
+    'Added responsive design for window size.',
   ],
   fixed: [
-    'Fixed the preview error when previewing the first time.',
-    'Fixed the preview error when previewing the left side of the screen.',
-    'Fixed the engraving file time issue.',
-    'Fixed the merge text path issue.',
+    'Fixed working area issue when "Open Bottom" turned on with dpi greater than 500.',
   ],
   changed: [
   ],
