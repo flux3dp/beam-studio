27cffe15cab2ab5e9a0e6a2e8a65a57336e32128
Dean Sung <dean@flux3dp.com>
2023-11-22 15:53:00 +0800

    2.2.12 alpha

--- src/implementations/changelog.ts
+++ src/implementations/changelog.ts
@@ -1,27 +1,37 @@
 import { IChangeLog } from 'interfaces/IChangeLog';
 
-// 2.1.4-alpha
+// 2.2.12-alpha
 const CHANGES_TW = {
   added: [
-    '新增「選擇機器」視窗。',
+    '新增紅外線雷射和列印模組的校正提醒。',
   ],
   fixed: [
-    '修正外框預覽連線錯誤時對話框不會關閉。',
+    '修正 CMYK 點陣圖顯示問題。',
+    '修正 RGB 點陣圖列印色差問題。',
+    '修正預噴區在刪除列印圖層後未正確刪除。',
+    '修正列印測試前未執行預噴動作。',
+    '修正匯入 PDF 及 AI 檔案時取消操作導致畫面當機的問題。',
+    '修正匯出成 SVG 格式時，圖片大小錯誤。',
   ],
   changed: [
-    '調整右側面板畫面樣式。',
+    '變更 Ador 校正工作時的風扇轉速為 10%。',
   ],
 };
 
 const CHANGES_EN = {
   added: [
-    'Add "Select Machine" modal.',
+    'Added a reminder of calibration for the infrared laser and printing module.',
   ],
   fixed: [
-    'Fixed framing issue when connection failed.',
+    'Fixed CMYK bitmap display issue.',
+    'Fixed RGB printing color issue.',
+    'Fixed the issue where the prespray area was not correctly deleted after removing a printing layer.',
+    'Fixed the issue of not performing pre-spray actions before printing test.',
+    'Fixed the problem where cancelling the operation while importing PDF and AI files caused the screen to freeze.',
+    'Fixed the issue of incorrect image sizes when exporting to SVG format.',
   ],
   changed: [
-    'Update Right Panel UI.',
+    'Changed the speed of ventilator to 10% while performing calibration task.',
   ],
 };
 
