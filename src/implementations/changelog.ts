86fe9ee93493282770180a2af7fd036d2790cade
Dean Sung <dean@flux3dp.com>
2023-12-12 16:07:23 +0800

    Update change log

--- src/implementations/changelog.ts
+++ src/implementations/changelog.ts
@@ -1,37 +1,37 @@
 import { IChangeLog } from 'interfaces/IChangeLog';
 
-// 2.2.12-alpha
+// 2.2.13-alpha
 const CHANGES_TW = {
   added: [
-    '新增紅外線雷射和列印模組的校正提醒。',
+    '新增 Ador 根據機器加速度調整加速度區間。',
+    '新增 送出 Ador 工作時韌體版本檢查，請使用 5.1.12 以上韌體版本以送出工作。',
+    '新增 波蘭文。',
   ],
   fixed: [
-    '修正 CMYK 點陣圖顯示問題。',
-    '修正 RGB 點陣圖列印色差問題。',
-    '修正預噴區在刪除列印圖層後未正確刪除。',
-    '修正列印測試前未執行預噴動作。',
-    '修正匯入 PDF 及 AI 檔案時取消操作導致畫面當機的問題。',
-    '修正匯出成 SVG 格式時，圖片大小錯誤。',
+    '修正 多次移動圖層後圖層不可點選的問題。',
+    '修正 Ador 影像描圖功能。',
+    '修正 Ador 工作範圍邊界的圖形不會被雕刻的問題。',
   ],
   changed: [
-    '變更 Ador 校正工作時的風扇轉速為 10%。',
+    '變更 Ador 紅外線模組工作範圍。',
+    '變更 部分西班牙文翻譯。',
   ],
 };
 
 const CHANGES_EN = {
   added: [
-    'Added a reminder of calibration for the infrared laser and printing module.',
+    'Added Ador acceleration area adjustment according to the machine setting.',
+    'Added firmware version check when sending task to Ador. Please use firmware version 5.1.12 or above to send task.',
+    'Added Polish language.',
   ],
   fixed: [
-    'Fixed CMYK bitmap display issue.',
-    'Fixed RGB printing color issue.',
-    'Fixed the issue where the prespray area was not correctly deleted after removing a printing layer.',
-    'Fixed the issue of not performing pre-spray actions before printing test.',
-    'Fixed the problem where cancelling the operation while importing PDF and AI files caused the screen to freeze.',
-    'Fixed the issue of incorrect image sizes when exporting to SVG format.',
+    'Fixed the issue that the layer cannot be selected after moving the layer multiple times.',
+    'Fixed the image tracing function for Ador.',
+    'Fixed the issue that the path on the boundary of Ador work area will not be engraved.',
   ],
   changed: [
-    'Changed the speed of ventilator to 10% while performing calibration task.',
+    'Changed the work area of Ador infrared module.',
+    'Changed part of the Spanish translation.',
   ],
 };
 
