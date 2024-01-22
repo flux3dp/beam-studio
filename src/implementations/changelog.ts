9a3bf2c10262c4bd87a6a6fed5e9ef6cbf712f72
Dean Sung <dean@flux3dp.com>
2024-01-22 12:28:18 +0800

    Updates from ador build (#288)
    * Update version and publish path
    
    * 2.2.11 alpha
    
    * 2.2.12 alpha
    
    * 2.2.12 beta
    
    * 2.2.13 alpha
    
    * Test build 2.2.13 alpha
    
    * Update ghost & dependency version
    
    * Update change log
    
    * Fix single color example file
    
    * 2.2.14 alpha
    
    * Change fluxclient version to 2.5.8
    
    * Update fluxsvg to 2.7.3
    
    * Update change log for 2.2.14
    
    * 2.2.15 alpha
    
    * Update ghost dependency version
    
    * Change back to default publish path

--- src/implementations/changelog.ts
+++ src/implementations/changelog.ts
@@ -1,37 +1,35 @@
 import { IChangeLog } from 'interfaces/IChangeLog';
 
-// 2.2.12-alpha
+// 2.2.14-alpha
 const CHANGES_TW = {
   added: [
-    '新增紅外線雷射和列印模組的校正提醒。',
+    '新增 多國語翻譯。',
+    '新增 支援換頭鎖 Z 軸馬達。',
   ],
   fixed: [
-    '修正 CMYK 點陣圖顯示問題。',
-    '修正 RGB 點陣圖列印色差問題。',
-    '修正預噴區在刪除列印圖層後未正確刪除。',
-    '修正列印測試前未執行預噴動作。',
-    '修正匯入 PDF 及 AI 檔案時取消操作導致畫面當機的問題。',
-    '修正匯出成 SVG 格式時，圖片大小錯誤。',
+    '修正 部分語言翻譯。',
+    '修正 SVG 匯入相關問題。',
+    '修正 刪除列印圖層後復原，預噴區圖案未復原。',
+    '修正 匯入含有點陣圖的 SVG 時，點陣圖層參數未正確初始化。',
   ],
   changed: [
-    '變更 Ador 校正工作時的風扇轉速為 10%。',
+    '變更 Ador 新增機器圖片。',
   ],
 };
 
 const CHANGES_EN = {
   added: [
-    'Added a reminder of calibration for the infrared laser and printing module.',
+    'Added multi-language support.',
+    'Added support for locking the Z-axis motor when changing module.',
   ],
   fixed: [
-    'Fixed CMYK bitmap display issue.',
-    'Fixed RGB printing color issue.',
-    'Fixed the issue where the prespray area was not correctly deleted after removing a printing layer.',
-    'Fixed the issue of not performing pre-spray actions before printing test.',
-    'Fixed the problem where cancelling the operation while importing PDF and AI files caused the screen to freeze.',
-    'Fixed the issue of incorrect image sizes when exporting to SVG format.',
+    'Fixed some translations.',
+    'Fixed importing SVG related issues.',
+    'Fixed the issue that pre-spary area is not restored after undoing the deletion of printing layer.',
+    'Fixed the issue that the parameters of bitmap layer are not correctly initialized when importing SVG with bitmap.',
   ],
   changed: [
-    'Changed the speed of ventilator to 10% while performing calibration task.',
+    'Changed the images in Ador setup pages.',
   ],
 };
 
