ff3edc8f73c77e25e6f9778ebe0a59e467969c33
Dean Sung <dean@flux3dp.com>
2024-01-29 16:30:15 +0800

    Update changelog

--- src/implementations/changelog.ts
+++ src/implementations/changelog.ts
@@ -1,35 +1,41 @@
 import { IChangeLog } from 'interfaces/IChangeLog';
 
-// 2.2.14-alpha
+// 2.2.15-alpha
 const CHANGES_TW = {
   added: [
-    '新增 多國語翻譯。',
-    '新增 支援換頭鎖 Z 軸馬達。',
+    '新增 非工作區域文字。',
   ],
   fixed: [
-    '修正 部分語言翻譯。',
-    '修正 SVG 匯入相關問題。',
-    '修正 刪除列印圖層後復原，預噴區圖案未復原。',
-    '修正 匯入含有點陣圖的 SVG 時，點陣圖層參數未正確初始化。',
+    '修正 「檢視」裡的勾選狀態。',
+    '修正 多物件無法同時移動圖層。',
+    '修正 勾勒輪廓時卡頓問題。',
+    '修正 匯入 PDF 檔案問題。',
+    '修正 匯入 SVG 檔案問題。',
+    '修正 相機校正進度條顯示。',
+    '修正 列印預噴區取消遮蔽後未正常復原。',
   ],
   changed: [
-    '變更 Ador 新增機器圖片。',
+    '變更 物件群組邏輯。',
+    '變更 Ador 最小加速度區間。',
   ],
 };
 
 const CHANGES_EN = {
   added: [
-    'Added multi-language support.',
-    'Added support for locking the Z-axis motor when changing module.',
+    'Added non-workarea text.',
   ],
   fixed: [
-    'Fixed some translations.',
-    'Fixed importing SVG related issues.',
-    'Fixed the issue that pre-spary area is not restored after undoing the deletion of printing layer.',
-    'Fixed the issue that the parameters of bitmap layer are not correctly initialized when importing SVG with bitmap.',
+    'Fixed check status in the View menu.',
+    'Fixed the issue where multiple selected objects could not be moved to another layer.',
+    'Fixed the lag issue when performing image outlining.',
+    'Fixed the issue with PDF file importing.',
+    'Fixed the issue with SVG file importing.',
+    'Fixed the display of the camera calibration progress bar.',
+    'Fixed the pre-spray area after undoing the hiding layer.',
   ],
   changed: [
-    'Changed the images in Ador setup pages.',
+    'Changed the behavior of grouping objects.',
+    'Changed the minimum engraving padding for Ador.',
   ],
 };
 
