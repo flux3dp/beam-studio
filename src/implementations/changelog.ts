59fb78ecd2f5c61626d5a6a100718709f2b0bd84
EstherFu <sloborfu@gmail.com>
2024-02-06 18:38:21 +0800

    Build 2.3.0 alpha

--- src/implementations/changelog.ts
+++ src/implementations/changelog.ts
@@ -1,36 +1,28 @@
 import { IChangeLog } from 'interfaces/IChangeLog';
 
-// 2.2.14-alpha
+// 2.3.0-alpha
 const CHANGES_TW = {
   added: [
-    '新增 多國語翻譯。',
-    '新增 支援換頭鎖 Z 軸馬達。',
+    '新增 訂閱制功能。',
+    '新增 雲端空間。',
+    '新增 盒子產生器。',
+    // web font
+    '新增 字體搜尋功能。',
   ],
-  fixed: [
-    '修正 部分語言翻譯。',
-    '修正 SVG 匯入相關問題。',
-    '修正 刪除列印圖層後復原，預噴區圖案未復原。',
-    '修正 匯入含有點陣圖的 SVG 時，點陣圖層參數未正確初始化。',
-  ],
-  changed: [
-    '變更 Ador 新增機器圖片。',
-  ],
+  fixed: [],
+  changed: [],
 };
 
 const CHANGES_EN = {
   added: [
-    'Added multi-language support.',
-    'Added support for locking the Z-axis motor when changing module.',
+    'Added FLUX+ features.',
+    'Added My Cloud.',
+    'Added Box generator.',
+    // web font
+    'Added Font family filter.',
   ],
-  fixed: [
-    'Fixed some translations.',
-    'Fixed importing SVG related issues.',
-    'Fixed the issue that pre-spary area is not restored after undoing the deletion of printing layer.',
-    'Fixed the issue that the parameters of bitmap layer are not correctly initialized when importing SVG with bitmap.',
-  ],
-  changed: [
-    'Changed the images in Ador setup pages.',
-  ],
+  fixed: [],
+  changed: [],
 };
 
 export default {
