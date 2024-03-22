3e09025fa694c2c7e7d9287e8d889b2e9ad06f75
Dean Sung <dean@flux3dp.com>
2024-03-22 10:34:42 +0800

    Merge 2.3.0 into develop
    commit fad102aa89a83a9c15265f377dc5ed989a1b9fce
    Author: EstherFu <sloborfu@gmail.com>
    Date:   Wed Mar 20 18:47:51 2024 +0800
    
        Update beta version
    
    commit 1bfd98bfa4570410ba01896cf47234de0d6cbf03
    Author: EstherFu <sloborfu@gmail.com>
    Date:   Wed Mar 20 18:43:39 2024 +0800
    
        Update changelog
    
    commit d4440f39dac5cfbe4f72bf26164ebd300a46597d
    Merge: c3bb539d a11f6a55
    Author: EstherFu <sloborfu@gmail.com>
    Date:   Mon Mar 18 15:22:47 2024 +0800
    
        Merge commit 'a11f6a5532713d9b94bb5add56e0c7ad80e029c0' into 2.3.0
    
    commit c3bb539de5b3bbc15922d1d7e65f753e23d0c457
    Author: EstherFu <95398751+EstherFu@users.noreply.github.com>
    Date:   Thu Mar 14 11:09:38 2024 +0800
    
        Remove menu icon and update changelog (#298)
    
        * Remove flux plus icon in menu
    
        * Update changelog
    
    commit 67e41d32a3eb6b4b50590e7ff895e7c5ae33ea7d
    Author: EstherFu <95398751+EstherFu@users.noreply.github.com>
    Date:   Thu Feb 29 10:24:27 2024 +0800
    
        Move web font (#296)
    
        * Update fontkit
    
        * Move to core
    
        * Add local font handler
    
        * Fix eslint and add type
    
    commit 7e9661436a748c381b84349832817bfaf404c04e
    Author: EstherFu <sloborfu@gmail.com>
    Date:   Thu Feb 15 14:05:42 2024 +0800
    
        Update yarn.lock
    
    commit caa1f420fedf9919931c66f7654aff092dc65235
    Author: Dean Sung <dean@flux3dp.com>
    Date:   Wed Feb 7 11:19:31 2024 +0800
    
        Update core branch
    
    commit c49ba8d904f21b8b5843668d28698c4e081f19f7
    Author: Dean Sung <dean@flux3dp.com>
    Date:   Wed Feb 7 11:18:33 2024 +0800
    
        2.3.0a

--- src/implementations/changelog.ts
+++ src/implementations/changelog.ts
@@ -1,35 +1,45 @@
 import { IChangeLog } from 'interfaces/IChangeLog';
 
-// 2.2.14-alpha
+// 2.3.0-beta
 const CHANGES_TW = {
   added: [
-    '新增 多國語翻譯。',
-    '新增 支援換頭鎖 Z 軸馬達。',
+    '新增 文字轉路徑 2.0。',
+    '新增 我的雲端功能。',
+    '新增 Boxgen 功能。',
+    '新增 網頁字型。',
+    '新增 字型搜尋功能。',
+    '新增 帳戶圖示和彈出視窗。',
+    '新增 錯誤代碼的文章連結於錯誤代碼視窗。',
+    '新增 活動推播通知。',
+    '新增 在右鍵點擊物件後移動圖層的選項。',
   ],
   fixed: [
-    '修正 部分語言翻譯。',
-    '修正 SVG 匯入相關問題。',
-    '修正 刪除列印圖層後復原，預噴區圖案未復原。',
-    '修正 匯入含有點陣圖的 SVG 時，點陣圖層參數未正確初始化。',
+    '修正 使用鉛筆工具後點擊圖層面板沒有反應的問題。',
   ],
   changed: [
-    '變更 Ador 新增機器圖片。',
+    '變更 自動儲存檔案的命名規則。',
+    '變更 將帳戶資料改為獨立選單。',
   ],
 };
 
 const CHANGES_EN = {
   added: [
-    'Added multi-language support.',
-    'Added support for locking the Z-axis motor when changing module.',
+    'Added Text to Path Converter 2.0.',
+    'Added My cloud function.',
+    'Added Boxgen function.',
+    'Added Web font.',
+    'Added Font search function.',
+    'Added Account icon and modal.',
+    'Added Error code article link.',
+    'Added Event push notification.',
+    'Added Option to move layers after right-clicking on object.',
   ],
   fixed: [
-    'Fixed some translations.',
-    'Fixed importing SVG related issues.',
-    'Fixed the issue that pre-spary area is not restored after undoing the deletion of printing layer.',
-    'Fixed the issue that the parameters of bitmap layer are not correctly initialized when importing SVG with bitmap.',
+    'Fixed "Layers" button doesn’t show the layer panel after Pen tool was used.',
   ],
   changed: [
-    'Changed the images in Ador setup pages.',
+    'Changed the naming of automatically saved files.',
+    'Changed the Account content to be an independent menu.',
   ],
 };
 
