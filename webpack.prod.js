a9694ac9a25af44df9e4c676d19bda940c14f19b
Dean Sung <dean@flux3dp.com>
2024-04-19 19:05:28 +0800

    Add sentryWebpackPlugin

---
+++ webpack.prod.js
@@ -0,0 +1,19 @@
+const { merge } = require('webpack-merge');
+const config = require('./webpack.config.js');
+const { sentryWebpackPlugin } = require("@sentry/webpack-plugin");
+
+const prodConfig = config.map((c) => {
+
+  return merge(c, {
+    mode: 'production',
+    plugins: c.entry === './src/main.ts' ? [
+      sentryWebpackPlugin({
+        org: "flux3dp",
+        project: "beam-studio",
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+      }),
+    ] : undefined,
+  });
+});
+
+export default prodConfig;
