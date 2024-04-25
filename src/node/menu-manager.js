1fca9c6cb1e574c724187fcf1da8ca0b465222e0
Dean Sung <dean@flux3dp.com>
2024-04-25 16:25:35 +0800

    Remove camera calibration v2 menu item

--- src/node/menu-manager.js
+++ src/node/menu-manager.js
@@ -400,21 +400,12 @@ function buildDeviceMenu(callback, uuid, data, isDevMode = false) {
     } else if (modelType === 'ador-series') {
       if (isDevMode) {
         submenu.push({
-          id: 'CALIBRATE_CAMERA_V2',
-          uuid,
-          serial,
-          machineName,
-          source,
-          label: r.calibrate_camera_v2,
-          click: callback,
-        });
-        submenu.push({
           id: 'CALIBRATE_CAMERA_V2_FACTORY',
           uuid,
           serial,
           machineName,
           source,
-          label: `${r.calibrate_camera_v2} (Factory)`,
+          label: `${r.calibrate_camera} (Factory)`,
           click: callback,
         });
       }
