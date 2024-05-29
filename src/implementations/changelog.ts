1bb7a1930a112e04b7c498c43c7ddc15460ffd4c
Dean Sung <dean@flux3dp.com>
2024-05-29 16:18:05 +0800

    2.3.5 beta

--- src/implementations/changelog.ts
+++ src/implementations/changelog.ts
@@ -1,43 +1,35 @@
 import { IChangeLog } from 'interfaces/IChangeLog';
 
-// 2.3.2-beta
+// 2.3.5-beta
 const CHANGES_TW = {
   added: [
-    '新增 Ador 相機曝光值調整。',
-    '新增 Ador 外框預覽時弱出光',
-    '新增 Ador 列印半色調選項。',
-    '新增 韌體更新提示。',
+    '新增 windows 系統在路徑預覽時可以以滑鼠或觸控板拖曳畫面。',
   ],
   fixed: [
-    '修正 材質參數選單的位置。',
-    '修正 多邊形元素在變更邊長時位置會被移到左上方的問題。',
-    '修正 計算工作時間造成視窗卡住。',
-    '改善 Ador 列印顏色偏深的問題。',
-    '修正 2.3.2 beta 版本中 Ador 路徑加速度限制套用到雕刻上。',
+    '修正部分 復原問題。',
+    '修正 旋轉後的物件使用 (Alt / Option) + 拖曳複製，方向會錯誤。',
+    '修正 有尖角的元素在位移複製後出現倒角。',
+    '修正 右鍵選單的群組/解散群組選項沒有被正確觸發。',
+    '修正 路徑計算中「從這裡開始」的速度。',
   ],
   changed: [
-    '變更 Ador 相機校正流程。',
-    '變更 Ador 切割路徑的移動速度。',
+    '變更 Ador 校正流程中部分敘述。',
   ],
 };
 
 const CHANGES_EN = {
   added: [
-    'Added Ador camera exposure value (requires firmware 5.1.19 or above)',
-    'Added Ador low laser while frame preview.',
-    'Added Ador halftone option for printing.',
-    'Added firmware update reminded.',
+    'Added drag control with the mouse or trackpad in the path preview for Windows systems.',
   ],
   fixed: [
-    'Fixed the position of the material parameter drop down menu.',
-    'Fixed the issue where the polygon element would be moved to the top left corner when its changed.',
-    'Fixed the issue where estimate times causes the window to freeze.',
-    'Improved the issue with Ador prints being too dark.',
-    'Fixed the issue where path acceleration limits for Ador were applied to engraving in version 2.3.2 beta.',
+    'Fixed some undo issues.',
+    'Fixed the issue where the direction of the rotated object is incorrect when using (Alt / Option) + drag to copy.',
+    'Fixed the issue where the chamfer appeared after offsetting the element with sharp corners.',
+    'Fixed the issue where the group/ungroup options in the right-click menu were not enabled correctly.',
+    'Improved the performance of the "Start Here" button in the path preview.',
   ],
   changed: [
-    'Changed the Camera Calibration process for Ador.',
-    'Changed the travel speed of cutting path for Ador.',
+    'Changed some description in Camera Calibration process for Ador.',
   ],
 };
 
