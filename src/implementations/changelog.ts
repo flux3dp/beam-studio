39fe80884a4694e5fd93574dbf05875396d4aae7
Dean Sung <dean@flux3dp.com>
2024-08-15 18:10:18 +0800

    Update change log

--- src/implementations/changelog.ts
+++ src/implementations/changelog.ts
@@ -1,33 +1,33 @@
 import { IChangeLog } from 'interfaces/IChangeLog';
 
-// 2.3.8-beta
+// 2.3.9-beta
 const CHANGES_TW = {
   added: [
-    '新增 自動對位功能。',
-    '新增 支援 Ador 旋轉軸 (需搭配韌體 v5.3.3)',
+    '新增 自動對位功能 (Beta)。',
+    '新增 漸層圖片的深度模式。',
+    '新增 自動切換圖層與物件面板選項於偏好設定。',
+    '新增 相機預覽拼接照片時的羽化效果。',
   ],
   fixed: [
-    '修正 點陣圖旋轉特定角度後送出工作位置錯誤。',
-    '修正 部分圖檔直角計算錯誤問題。',
   ],
   changed: [
-    '自動記住第三方登入資訊',
-    '將「自動排列」更名為「智慧排版」',
+    '在物件面板上的輸入欄位可使用逗號作為小數點。',
+    '改善計算多圖層工作路徑時記憶體使用量。',
   ],
 };
 
 const CHANGES_EN = {
   added: [
-    'Added Auto Fit feature.',
-    'Added support for Ador Rotary (need to use firmware v5.3.3)',
+    'Added Auto Fit feature (Beta).',
+    'Added Depth Mode for gradient image.',
+    'Added an option to automatically switch layers and object panels in the preferences.',
+    'Added feathering effect when stitching photos in the camera preview.',
   ],
   fixed: [
-    'Fixed issue with incorrect positions after rotating images by specific angles.',
-    'Fixed calculation error for right angles in some image files.',
   ],
   changed: [
-    'Auto-save third-party login information',
-    'Renamed “Auto Arrange” to “Smart Nest”',
+    'The input fields in the object panel can use a comma as a decimal point.',
+    'Improve memory usage when calculating task path with lots of layer.',
   ],
 };
 
