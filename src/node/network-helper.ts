b1362772dfcd8a672bb835e4e1c241499631600a
Dean Sung <dean@flux3dp.com>
2024-08-22 12:18:46 +0800

    Replace ping module

--- src/node/network-helper.ts
+++ src/node/network-helper.ts
@@ -1,117 +1,57 @@
-/* eslint-disable no-param-reassign */
-import ping from 'net-ping';
+import ping from 'ping';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { BrowserWindow, ipcMain } from 'electron';
 
 import events from './ipc-events';
 
 const testNetwork = async (ip: string, time: number, mainWindow: BrowserWindow) => {
-  let session: Session;
-  // eslint-disable-next-line no-async-promise-executor
-  return new Promise(async (resolve) => {
-    const createSession = () => {
-      try {
-        session = ping.createSession({
-          retries: 0,
-        });
-        session.on('error', (error) => {
-          // eslint-disable-next-line no-console
-          console.log(`session error: ${error}`);
-          createSession();
-        });
-      } catch (e) {
-        resolve({ err: 'CREATE_SESSION_FAILED', reason: String(e) });
-        throw e;
-      }
-    };
-    createSession();
-    if (!session) return;
-    const start = Date.now();
-    let pingCount = 0;
-    let successCount = 0;
-    let totalRRT = 0;
-    const doPing = () =>
-      new Promise<{ error?: string; rrt?: number }>((resolve2) => {
-        let resolved = false;
-        setTimeout(() => {
-          if (!resolved) {
-            resolve2({ error: 'TOMEOUT' });
-            resolved = true;
-          }
-        }, 3000);
-        session.pingHost(ip, (error, target, sent, rcvd) => {
-          if (!resolved) {
-            if (error) {
-              // console.log(`${target}: ${error.toString()}`);
-              if (error.toString().match('Invalid IP address')) {
-                resolve2({ error: 'INVALID_IP' });
-              } else {
-                resolve2({ error: error.toString() });
-              }
-            } else {
-              resolve2({ rrt: rcvd - sent });
-            }
-          }
-          resolved = true;
-        });
-      });
-    while (Date.now() < start + time) {
-      const elapsedTime = Date.now() - start;
-      const percentage = Math.round((100 * elapsedTime) / time);
-      mainWindow.webContents.send(events.TEST_NETWORK_PROGRESS, percentage);
-      // eslint-disable-next-line no-await-in-loop
-      const { error, rrt } = await doPing();
-      pingCount += 1;
-      if (rrt) {
-        successCount += 1;
-        totalRRT += rrt;
-      } else if (error === 'INVALID_IP') {
-        resolve({ err: 'INVALID_IP' });
-        break;
-      }
-    }
-    const avgRRT = totalRRT / successCount;
-    const failedCount = pingCount - successCount;
-    const quality = avgRRT < 2 ? 100 - failedCount : 100 - 3 * failedCount;
-    resolve({
-      successRate: successCount / pingCount,
-      avgRRT,
-      quality,
+  const start = Date.now();
+  let pingCount = 0;
+  let successCount = 0;
+  let totalRRT = 0;
+  while (Date.now() < start + time) {
+    const elapsedTime = Date.now() - start;
+    const percentage = Math.round((100 * elapsedTime) / time);
+    mainWindow.webContents.send(events.TEST_NETWORK_PROGRESS, percentage);
+    // wait for the network to be ready
+    pingCount += 1;
+    // eslint-disable-next-line no-await-in-loop
+    const res = await ping.promise.probe(ip, {
+      timeout: 3,
     });
-  });
+
+    const { alive, time: rrt } = res;
+    if (alive && rrt !== 'unknown') {
+      successCount += 1;
+      totalRRT += rrt;
+    }
+  }
+  const avgRRT = totalRRT / successCount;
+  const failedCount = pingCount - successCount;
+  const quality = avgRRT < 2 ? 100 - failedCount : 100 - 3 * failedCount;
+  return {
+    successRate: successCount / pingCount,
+    avgRRT,
+    quality,
+  };
 };
 
 const checkIPExist = async (ip: string, trial: number) => {
   try {
-    const session = ping.createSession();
-    session.on('error', (error) => {
-      throw error;
-    });
-    const res = await new Promise<{ isExisting: boolean }>((resolve) => {
-      let failed = 0;
-      const doPing = () => {
-        session.pingHost(ip, (error) => {
-          if (!error) {
-            // console.log('rrt', rcvd - sent);
-            resolve({ isExisting: true });
-            return;
-          }
-          // console.log(error);
-          failed += 1;
-          if (failed >= trial) {
-            resolve({ isExisting: false });
-          } else {
-            doPing();
-          }
-        });
-      };
-      doPing();
-    });
-    return res;
-  } catch (err) {
+    for (let i = 0; i < trial; i += 1) {
+      // eslint-disable-next-line no-await-in-loop
+      const res = await ping.promise.probe(ip, {
+        timeout: 3,
+      });
+      if (res.alive) {
+        return { isExisting: true };
+      }
+    }
+    return { isExisting: false };
+  } catch (error) {
     // eslint-disable-next-line no-console
-    console.log(err);
-    return { error: String(err) };
+    console.log(error);
+    return { error: String(error) };
   }
 };
 
@@ -120,6 +60,7 @@ const registerEvents = (mainWindow: BrowserWindow): void => {
   ipcMain.on(events.TEST_NETWORK, async (event, ip, time) => {
     const res = await testNetwork(ip, time, mainWindow);
     mainWindow.webContents.send(events.TEST_NETWORK_RESULT, res);
+    // eslint-disable-next-line no-param-reassign
     event.returnValue = res;
   });
 
@@ -127,6 +68,7 @@ const registerEvents = (mainWindow: BrowserWindow): void => {
   ipcMain.on(events.CHECK_IP_EXIST, async (event, ip, trial) => {
     const res = await checkIPExist(ip, trial);
     mainWindow.webContents.send(events.CHECK_IP_EXIST_RESULT, res);
+    // eslint-disable-next-line no-param-reassign
     event.returnValue = res;
   });
 };
