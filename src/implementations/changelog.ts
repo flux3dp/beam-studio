d65165cea495056946804516634737077a07c043
Dean Sung <dean@flux3dp.com>
2024-11-27 17:41:27 +0800

    Update change log

--- src/implementations/changelog.ts
+++ src/implementations/changelog.ts
@@ -1,44 +1,36 @@
 import { IChangeLog } from 'interfaces/IChangeLog';
 
-// 2.4.3 beta
+// 2.4.4 beta
 const CHANGES_TW = {
   added: [
-    '新增 點陣圖的梯形變形功能。',
-    '新增 支援 Promark 機型。',
-    '新增 條碼生成器。',
-    '新增 路徑計算加速選項於偏好設定 (Beta)。',
-    '新增 外框預覽（F1）與開始工作（F2) 快捷鍵。',
+    '新增 支援 Beambox II機型。',
+    '新增 Promark 機器設定時的參數設定畫面。',
+    '新增 Promark Mopa 材質測試工具的頻率與脈寬選項。',
+    '新增 Promark 相機校正動畫。',
   ],
   fixed: [
-    '修正 Ador 相機校正參數。',
-    '修正 命名圖層時使用快捷鍵儲存會造成上層選單無反應的問題。',
-    '修正 部分翻譯。',
-    '修正 「逐步對焦」在沒有勾選「降低焦距」時沒有作用。',
+    '修正 Promark 相關問題。',
+    '修正 可以在不同 Beam Studio App 中複製貼上物件。',
+    '修正 勾選偏好設定中「路徑計算加速」選項時的相關問題。',
+    '修正 SVG 物件在解散圖檔後會被填充。',
   ],
   changed: [
-    '複選物件時可以選到所有物件圖層。',
-    '調整機器設定的 USB 連線頁面。',
-    '調整 盒子產生器的切口補償預設值變更為 0.1mm。',
+    '變更 部分上層選單的分類方式。',
+    '變更 偏好設定中「相機預覽移動速度」的敘述與速度設定。',
   ],
 };
 
 const CHANGES_EN = {
   added: [
-    'Added “Rotary Warped” feature for bitmap image.',
-    'Added Barcode generator.',
-    'Added Path Calculation Acceleration option in preference (Beta).',
-    'Add shortcut keys for running frame (F1) and start work (F2).',
   ],
   fixed: [
-    'Fixed Ador camera calibration parameter',
-    'Fixed part of translation',
-    'Fixed an issue where using a shortcut to save while naming a layer caused the top menu to become unresponsive.',
-    'Fix the issue where “Stepwise Focusing” does not work when “Lower Focus”  is not selected.',
+    'Fixed an issue where objects could not be copied and pasted between different Beam Studio apps.',
+    'Fixed issues related to enabling the “Path Calculation Acceleration” option in preferences.',
+    'Fixed SVG objects being filled after disassembling.',
   ],
   changed: [
-    'Select all object layers when multiple objects are chosen.',
-    'Adjust USB connection page in machine setup.',
-    'Changed the default Kerf compensation in Box generator to 0.1mm',
+    'Updated the categorization of some top-level menu items.',
+    'Changed the description and speed setting of Camera’s “preview movement speed” in preference.',
   ],
 };
 
