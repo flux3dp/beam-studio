dfde4ffce0b80a95b07d2ea35c961b0b43710352
EstherFu <sloborfu@gmail.com>
2025-02-07 16:45:03 +0800

    Show social media invitation on fifth task

--- packages/core/src/web/helpers/alert-helper.ts
+++ packages/core/src/web/helpers/alert-helper.ts
@@ -1,21 +1,22 @@
+import alertCaller from '@core/app/actions/alert-caller';
+import dialogCaller from '@core/app/actions/dialog-caller';
 import alertConfig from '@core/helpers/api/alert-config';
 import eventEmitterFactory from '@core/helpers/eventEmitterFactory';
 import i18n from '@core/helpers/i18n';
 import browser from '@core/implementations/browser';
-import type { IAlert } from '@core/interfaces/IAlert';
 
-let isInvitationShowed = false;
-const showFacebookGroupInvitation = (popUp: (args: IAlert) => void) => {
+let count = 0;
+const showFacebookGroupInvitation = () => {
   const LANG = i18n.lang.beambox.popup.facebook_group_invitation;
 
-  if (!isInvitationShowed && !alertConfig.read('skip-fb-group-invitation')) {
+  if (!alertConfig.read('skip-fb-group-invitation')) {
     const handleJoinNow = () => {
       browser.open(i18n.lang.topbar.menu.link.forum);
       alertConfig.write('skip-fb-group-invitation', true);
     };
     const handleAlreadyJoined = () => alertConfig.write('skip-fb-group-invitation', true);
 
-    popUp({
+    alertCaller.popUp({
       buttonLabels: [LANG.join_now, LANG.later, LANG.already_joined],
       callbacks: [handleJoinNow, () => {}, handleAlreadyJoined],
       caption: LANG.title,
@@ -26,14 +27,24 @@ const showFacebookGroupInvitation = (popUp: (args: IAlert) => void) => {
       message: LANG.message,
       primaryButtonIndex: 0,
     });
-    isInvitationShowed = true;
   }
 };
 
-const registerAlertEvents = (popUp: (args: IAlert) => void): void => {
+const showMediaInvitation = (): void => {
+  if (count > 5) return;
+
+  count += 1;
+
+  if (count === 1) showFacebookGroupInvitation();
+  else if (count === 5 && !alertConfig.read('skip-social-media-invitation')) dialogCaller.showSocialMedia(true);
+};
+
+const registerAlertEvents = (): void => {
+  count = 0; // Reset count for testing
+
   const monitorEventEmitter = eventEmitterFactory.createEventEmitter('monitor');
 
-  monitorEventEmitter.on('PLAY', () => showFacebookGroupInvitation(popUp));
+  monitorEventEmitter.on('PLAY', showMediaInvitation);
 };
 
 export default {
