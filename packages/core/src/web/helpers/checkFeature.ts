28253b5a4797d1f88b5657726cf93339cd76eaa4
Dean Sung <dean@flux3dp.com>
2025-02-26 16:36:08 +0800

    Update for agent release

--- packages/core/src/web/helpers/checkFeature.ts
+++ packages/core/src/web/helpers/checkFeature.ts
@@ -1,8 +1,4 @@
-import isDev from './is-dev';
-import isWeb from './is-web';
-import localeHelper from './locale-helper';
-
-export const checkFbb2 = (): boolean => localeHelper.isTwOrHk || localeHelper.isJp || isDev();
-export const checkFbb2AutoFeeder = (): boolean => isDev();
-export const checkFpm1 = (): boolean => (localeHelper.isTwOrHk || isDev()) && !isWeb();
-export const checkChuckRotary = (): boolean => localeHelper.isTwOrHk || isDev();
+export const checkFbb2 = (): boolean => true;
+export const checkFbb2AutoFeeder = (): boolean => true;
+export const checkFpm1 = (): boolean => true;
+export const checkChuckRotary = (): boolean => true;
