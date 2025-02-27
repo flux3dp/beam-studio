113637f1830adb358349a6dd15b1829fde7a89ca
Dean Sung <dean@flux3dp.com>
2025-02-27 14:23:38 +0800

    Fix mobile element-tools

--- apps/web/cypress/e2e/mobile-web/element-tools.spec.ts
+++ apps/web/cypress/e2e/mobile-web/element-tools.spec.ts
@@ -11,7 +11,7 @@ describe('mobile element tools', () => {
   it('import ellipse element', () => {
     cy.get('.adm-tab-bar-item').contains('Element').click();
     cy.get('[class*="src-web-app-widgets-FloatingPanel-module__title--"]').contains('Element').should('exist');
-    cy.get(`.anticon[class*="${shapePanelPrefix}icon--"]`).eq(0).click();
+    cy.get(`.anticon[class*="${shapePanelPrefix}icon--"][id="basic/icon-circle"]`).eq(0).click();
     cy.get('#svg_1').should('exist');
     cy.get('#svg_1').should('have.attr', 'cx', '250');
     cy.get('#svg_1').should('have.attr', 'cy', '250');
@@ -23,11 +23,11 @@ describe('mobile element tools', () => {
   it('import svg element', () => {
     cy.get('.adm-tab-bar-item').contains('Element').click();
     cy.get('.adm-capsule-tabs-tab-wrapper').contains('Decor').click();
-    cy.get(`.anticon[class*="${shapePanelPrefix}icon--"]`).eq(0).click();
+    cy.get(`.anticon[class*="${shapePanelPrefix}icon--"][id="decor/i_circular-1"]`).eq(0).click();
     cy.get('#svg_9').should('exist').should('have.attr', 'fill', '#333333');
     cy.get('#svg_9')
       .invoke('attr', 'd')
-      .then((html) => expect(md5(html)).equal('7e57d924a29e7ad9272398d9e6595eec'));
+      .then((html) => expect(md5(html)).equal('9c67b02ead7460bfd1a668db6b7a1e0a'));
     cy.get('#selectorGrip_rotate_bottom').should('be.visible');
     cy.get('#selectorGrip_dimension_info').should('be.visible').should('have.text', '50mm x 38.3mm');
   });
