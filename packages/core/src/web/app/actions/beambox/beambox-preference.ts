37dcd0588d5457d9ccbb2cd518060d4c97585fac
Tuhacrt <a067800497@gmail.com>
2025-03-04 10:25:49 +0800

    feat(beambox-preference): update beambox-preference usage

--- packages/core/src/web/app/actions/beambox/beambox-preference.ts
+++ packages/core/src/web/app/actions/beambox/beambox-preference.ts
@@ -1,73 +1,194 @@
+import { PreviewSpeedLevel } from '@core/app/actions/beambox/constant';
 import constant from '@core/app/actions/beambox/constant';
+import { CHUCK_ROTARY_DIAMETER, RotaryType } from '@core/app/constants/add-on';
+import LayerModule from '@core/app/constants/layer-module/layer-modules';
+import moduleOffsets from '@core/app/constants/layer-module/module-offsets';
+import type { WorkAreaModel } from '@core/app/constants/workarea-constants';
 import eventEmitterFactory from '@core/helpers/eventEmitterFactory';
 import storage from '@core/implementations/storage';
+import type { Prettify } from '@core/interfaces/utils';
 
-const DEFAULT_PREFERENCE = {
+export type BeamboxPreference = {
+  'af-offset': number;
+  'anti-aliasing': boolean;
+  'auto-feeder': boolean;
+  'auto-feeder-height'?: number;
+  'auto-switch-tab': boolean;
+  auto_align: boolean;
+  blade_precut: boolean;
+  blade_radius: number;
+  borderless: boolean;
+  continuous_drawing: boolean;
+  curve_engraving_speed_limit: boolean;
+  'customized-dimension': Partial<Record<WorkAreaModel, { height: number; width: number }>>;
+  'default-autofocus': boolean;
+  'default-borderless': boolean;
+  'default-diode': boolean;
+  'default-laser-module': LayerModule;
+  'diode-one-way-engraving': boolean;
+  diode_offset_x: number;
+  diode_offset_y: number;
+  'enable-autofocus'?: boolean;
+  'enable-custom-backlash': boolean;
+  'enable-custom-preview-height': boolean;
+  'enable-diode'?: boolean;
+  'enable-job-origin': boolean;
+  enable_mask: boolean;
+  engrave_dpi: 'high' | 'low' | 'medium';
+  'extend-rotary-workarea': boolean;
+  fast_gradient: boolean;
+  'font-convert': '1.0' | '2.0';
+  'font-substitute': boolean;
+  'frame-before-start': boolean;
+  guide_x0: number;
+  guide_y0: number;
+  image_downsampling: boolean;
+  'import-module'?: LayerModule;
+  'job-origin': number;
+  'keep-preview-result': boolean;
+  low_power: number;
+  // model is default workarea model
+  model: WorkAreaModel;
+  'module-offsets': Record<LayerModule, [number, number]>;
+  mouse_input_device: 'MOUSE' | 'TOUCHPAD';
+  'multipass-compensation': boolean;
+  'one-way-printing': boolean;
+  padding_accel: number;
+  padding_accel_diode: number;
+  'pass-through': boolean;
+  'pass-through-height'?: number;
+  'path-engine': 'fluxghost' | 'swiftray';
+  precut_x: number;
+  precut_y: number;
+  preview_movement_speed_level: PreviewSpeedLevel;
+  'print-advanced-mode': boolean;
+  'promark-start-button': boolean;
+  'reverse-engraving': boolean;
+  'rotary-chuck-obj-d': number;
+  'rotary-mirror': boolean;
+  'rotary-type': RotaryType;
+  'rotary-y': null | number;
+  rotary_mode: boolean;
+  rotary_y_coord: number;
+  should_remind_calibrate_camera: boolean;
+  show_grids: boolean;
+  show_guides: boolean;
+  show_rulers: boolean;
+  simplify_clipper_path: boolean;
+  use_layer_color: boolean;
+  vector_speed_constraint: boolean;
+  workarea: WorkAreaModel;
+  zoom_with_window: boolean;
+};
+
+export type BeamboxPreferenceKey = Prettify<keyof BeamboxPreference>;
+export type BeamboxPreferenceValue<T extends BeamboxPreferenceKey> = BeamboxPreference[T];
+
+const DEFAULT_PREFERENCE: BeamboxPreference = {
+  'af-offset': 0,
   'anti-aliasing': true,
+  'auto-feeder': false,
+  'auto-switch-tab': false,
   auto_align: true,
+  blade_precut: false,
+  blade_radius: 0,
+  borderless: false,
+  continuous_drawing: false,
+  curve_engraving_speed_limit: true,
+  'customized-dimension': { fpm1: { height: 150, width: 150 } },
+  'default-autofocus': false,
+  'default-borderless': false,
+  'default-diode': false,
+  'default-laser-module': LayerModule.LASER_20W_DIODE,
+  'diode-one-way-engraving': true,
   diode_offset_x: constant.diode.defaultOffsetX,
   diode_offset_y: constant.diode.defaultOffsetY,
-  engrave_dpi: 'medium', // low, medium, high
+  'enable-custom-backlash': false,
+  'enable-custom-preview-height': false,
+  'enable-job-origin': false,
+  enable_mask: false,
+  engrave_dpi: 'medium',
+  'extend-rotary-workarea': false,
+  fast_gradient: true,
+  'font-convert': '2.0',
+  'font-substitute': true,
+  'frame-before-start': false,
   guide_x0: 0,
   guide_y0: 0,
+  image_downsampling: true,
+  'job-origin': 1,
+  'keep-preview-result': false,
   low_power: 10,
-  model: 'fbb1b',
+  model: 'fbm1',
+  'module-offsets': moduleOffsets,
   mouse_input_device: window.os === 'MacOS' ? 'TOUCHPAD' : 'MOUSE',
-  rotary_mode: 0,
+  'multipass-compensation': true,
+  'one-way-printing': true,
+  padding_accel: 5000,
+  padding_accel_diode: 4500,
+  'pass-through': false,
+  'path-engine': 'fluxghost',
+  precut_x: 0,
+  precut_y: 0,
+  preview_movement_speed_level: PreviewSpeedLevel.SLOW,
+  'print-advanced-mode': false,
+  'promark-start-button': false,
+  'reverse-engraving': false,
+  'rotary-chuck-obj-d': CHUCK_ROTARY_DIAMETER,
+  'rotary-mirror': false,
+  'rotary-type': RotaryType.Roller,
+  'rotary-y': null,
+  rotary_mode: false,
+  rotary_y_coord: 5,
   should_remind_calibrate_camera: true,
   show_grids: true,
-  show_guides: false,
+  show_guides: true,
+  show_rulers: false,
+  simplify_clipper_path: false,
   use_layer_color: true,
+  vector_speed_constraint: true,
+  workarea: 'fbb1b',
+  zoom_with_window: false,
 };
 
 const eventEmitter = eventEmitterFactory.createEventEmitter('beambox-preference');
 
-class BeamboxPreference {
+class BeamboxPreferenceClass {
   constructor() {
     // set default preference if key or even beambox-preference doesn't exist
-    let pref: any = storage.get('beambox-preference');
+    const preference: BeamboxPreference = storage.get('beambox-preference') || DEFAULT_PREFERENCE;
 
-    pref = pref === '' ? {} : pref;
-    console.log(pref);
+    // to migrate preference of old version
+    for (const key in DEFAULT_PREFERENCE) {
+      if (!(key in preference)) {
+        // @ts-expect-error the key is valid BeamboxPreferenceKey
+        preference[key] = DEFAULT_PREFERENCE[key];
+      }
+    }
 
-    const fullPref = Object.assign(DEFAULT_PREFERENCE, pref);
+    console.log('startup preference', preference);
 
-    storage.set('beambox-preference', fullPref);
+    storage.set('beambox-preference', preference);
   }
 
-  read(key: string) {
+  read<Key extends BeamboxPreferenceKey>(key: Key): BeamboxPreferenceValue<Key> {
     return storage.get('beambox-preference')[key];
   }
 
-  write(key: string, value: any) {
-    const pref = storage.get('beambox-preference');
+  write<Key extends BeamboxPreferenceKey>(key: Key, value: BeamboxPreferenceValue<Key>): void {
+    const preference = storage.get('beambox-preference');
 
-    pref[key] = value;
-    storage.set('beambox-preference', pref);
+    preference[key] = value;
+    storage.set('beambox-preference', preference);
     eventEmitter.emit(key, value);
   }
 }
 
-const beamboxPreference = new BeamboxPreference();
+const beamboxPreference = new BeamboxPreferenceClass();
 
 export const migrate = (): void => {
-  const rotaryMode = beamboxPreference.read('rotary_mode');
-
-  if (typeof rotaryMode === 'boolean') {
-    beamboxPreference.write('rotary_mode', rotaryMode ? 1 : 0);
-  }
-
-  if (beamboxPreference.read('model') === 'fad1') {
-    beamboxPreference.write('model', 'ado1');
-  }
-
-  if (beamboxPreference.read('workarea') === 'fad1') {
-    beamboxPreference.write('workarea', 'ado1');
-  }
-
-  if (beamboxPreference.read('auto_align') === undefined) {
-    beamboxPreference.write('auto_align', true);
-  }
+  beamboxPreference.write('rotary_mode', Boolean(beamboxPreference.read('rotary_mode')));
+  beamboxPreference.write('enable-job-origin', Boolean(beamboxPreference.read('rotary_mode')));
 };
 
 export default beamboxPreference;
