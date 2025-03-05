ae5d4bca75e3dfd5d538dab71c51ea6b368e7506
Dean Sung <dean@flux3dp.com>
2025-03-05 18:29:27 +0800

    2.5.0 agent

--- packages/core/src/web/helpers/checkFeature.ts
+++ packages/core/src/web/helpers/checkFeature.ts
@@ -1,6 +1,2 @@
-import isDev from './is-dev';
-import isWeb from './is-web';
-import localeHelper from './locale-helper';
-
-export const checkFbb2AutoFeeder = (): boolean => isDev();
-export const checkFpm1 = (): boolean => (localeHelper.isTwOrHk || isDev()) && !isWeb();
+export const checkFbb2AutoFeeder = (): boolean => true;
+export const checkFpm1 = (): boolean => true;
