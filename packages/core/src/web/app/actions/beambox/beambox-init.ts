317aaf0767a0ac57ff6543daa1209998dbf0206a
Dean Sung <dean@flux3dp.com>
2025-03-13 15:30:40 +0800

    Move openBottomBoundary to addOnBoundaryDrawer

--- packages/core/src/web/app/actions/beambox/beambox-init.ts
+++ packages/core/src/web/app/actions/beambox/beambox-init.ts
@@ -3,7 +3,6 @@ import BeamboxPreference, { migrate } from '@core/app/actions/beambox/beambox-pr
 import Constant from '@core/app/actions/beambox/constant';
 import Tutorials from '@core/app/actions/beambox/tutorials';
 import addOnBoundaryDrawer from '@core/app/actions/canvas/addOnBoundaryDrawer';
-import openBottomBoundaryDrawer from '@core/app/actions/canvas/openBottomBoundaryDrawer';
 import Dialog from '@core/app/actions/dialog-caller';
 import MessageCaller, { MessageLevel } from '@core/app/actions/message-caller';
 import { showAdorCalibrationV2 } from '@core/app/components/dialogs/camera/AdorCalibrationV2';
@@ -97,7 +96,6 @@ class BeamboxInit {
     BeamboxStore.onDrawGuideLines(this.displayGuides);
     alertHelper.registerAlertEvents();
     addOnBoundaryDrawer.registerEvents();
-    openBottomBoundaryDrawer.registerEvents();
 
     // WebSocket for Adobe Illustrator Plug-In
     aiExtension.init();
