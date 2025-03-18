c995a6e93e48dc7e242d2aeeb18ad11d9094bb23
Dean Sung <dean@flux3dp.com>
2025-03-18 13:01:37 +0800

    Support fpm1 for ps & my (#402)

--- packages/core/src/web/helpers/locale-helper.spec.ts
+++ packages/core/src/web/helpers/locale-helper.spec.ts
@@ -23,7 +23,7 @@ describe('test locale-helper', () => {
       jest.spyOn(Date.prototype, 'getTimezoneOffset').mockReturnValue(300);
       mockParse.mockReturnValue({ region: 'US' });
       expect(localeHelper.detectNorthAmerica()).toBe(true);
-      expect(mockConsoleError).not.toBeCalled();
+      expect(mockConsoleError).not.toHaveBeenCalled();
     });
 
     test('when locale is en-US and timezone offset is 660', () => {
@@ -33,7 +33,7 @@ describe('test locale-helper', () => {
       jest.spyOn(Date.prototype, 'getTimezoneOffset').mockReturnValue(660);
       mockParse.mockReturnValue({ region: 'US' });
       expect(localeHelper.detectNorthAmerica()).toBe(false);
-      expect(mockConsoleError).not.toBeCalled();
+      expect(mockConsoleError).not.toHaveBeenCalled();
     });
 
     test('when locale is en-CA and timezone offset is 300', () => {
@@ -43,7 +43,7 @@ describe('test locale-helper', () => {
       jest.spyOn(Date.prototype, 'getTimezoneOffset').mockReturnValue(300);
       mockParse.mockReturnValue({ region: 'CA' });
       expect(localeHelper.detectNorthAmerica()).toBe(true);
-      expect(mockConsoleError).not.toBeCalled();
+      expect(mockConsoleError).not.toHaveBeenCalled();
     });
 
     test('when locale is en-CA and timezone offset is 660', () => {
@@ -53,7 +53,7 @@ describe('test locale-helper', () => {
       jest.spyOn(Date.prototype, 'getTimezoneOffset').mockReturnValue(660);
       mockParse.mockReturnValue({ region: 'CA' });
       expect(localeHelper.detectNorthAmerica()).toBe(false);
-      expect(mockConsoleError).not.toBeCalled();
+      expect(mockConsoleError).not.toHaveBeenCalled();
     });
 
     test('when locale is zh-CN and timezone offset is 300', () => {
@@ -63,7 +63,7 @@ describe('test locale-helper', () => {
       jest.spyOn(Date.prototype, 'getTimezoneOffset').mockReturnValue(300);
       mockParse.mockReturnValue({ region: 'CN' });
       expect(localeHelper.detectNorthAmerica()).toBe(false);
-      expect(mockConsoleError).not.toBeCalled();
+      expect(mockConsoleError).not.toHaveBeenCalled();
     });
 
     test('when locale is zh-CN and timezone offset is 660', () => {
@@ -73,7 +73,7 @@ describe('test locale-helper', () => {
       jest.spyOn(Date.prototype, 'getTimezoneOffset').mockReturnValue(660);
       mockParse.mockReturnValue({ region: 'CN' });
       expect(localeHelper.detectNorthAmerica()).toBe(false);
-      expect(mockConsoleError).not.toBeCalled();
+      expect(mockConsoleError).not.toHaveBeenCalled();
     });
   });
 
@@ -85,7 +85,7 @@ describe('test locale-helper', () => {
       jest.spyOn(Date.prototype, 'getTimezoneOffset').mockReturnValue(-480);
       mockParse.mockReturnValue({ region: 'TW' });
       expect(localeHelper.detectTwOrHk()).toBe(true);
-      expect(mockConsoleError).not.toBeCalled();
+      expect(mockConsoleError).not.toHaveBeenCalled();
     });
 
     test('when locale is zh-HK', () => {
@@ -95,7 +95,7 @@ describe('test locale-helper', () => {
       jest.spyOn(Date.prototype, 'getTimezoneOffset').mockReturnValue(-480);
       mockParse.mockReturnValue({ region: 'HK' });
       expect(localeHelper.detectTwOrHk()).toBe(true);
-      expect(mockConsoleError).not.toBeCalled();
+      expect(mockConsoleError).not.toHaveBeenCalled();
     });
 
     test('when locale is zh-CN', () => {
@@ -105,7 +105,7 @@ describe('test locale-helper', () => {
       jest.spyOn(Date.prototype, 'getTimezoneOffset').mockReturnValue(-480);
       mockParse.mockReturnValue({ region: 'CN' });
       expect(localeHelper.detectTwOrHk()).toBe(false);
-      expect(mockConsoleError).not.toBeCalled();
+      expect(mockConsoleError).not.toHaveBeenCalled();
     });
   });
 
@@ -117,7 +117,7 @@ describe('test locale-helper', () => {
       jest.spyOn(Date.prototype, 'getTimezoneOffset').mockReturnValue(-540);
       mockParse.mockReturnValue({ language: 'ja' });
       expect(localeHelper.detectJp()).toBe(true);
-      expect(mockConsoleError).not.toBeCalled();
+      expect(mockConsoleError).not.toHaveBeenCalled();
     });
 
     test('when locale is ja-JP', () => {
@@ -127,7 +127,51 @@ describe('test locale-helper', () => {
       jest.spyOn(Date.prototype, 'getTimezoneOffset').mockReturnValue(-540);
       mockParse.mockReturnValue({ region: 'JP' });
       expect(localeHelper.detectJp()).toBe(true);
-      expect(mockConsoleError).not.toBeCalled();
+      expect(mockConsoleError).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('test isPs', () => {
+    test('when locale is ar', () => {
+      const navigator = { language: 'ar' };
+
+      Object.defineProperty(global, 'navigator', { value: navigator });
+      jest.spyOn(Date.prototype, 'getTimezoneOffset').mockReturnValue(-120);
+      mockParse.mockReturnValue({ language: 'ar' });
+      expect(localeHelper.detectPs()).toBe(true);
+      expect(mockConsoleError).not.toHaveBeenCalled();
+    });
+
+    test('when locale is ar-PS', () => {
+      const navigator = { language: 'ar-PS' };
+
+      Object.defineProperty(global, 'navigator', { value: navigator });
+      jest.spyOn(Date.prototype, 'getTimezoneOffset').mockReturnValue(-120);
+      mockParse.mockReturnValue({ region: 'PS' });
+      expect(localeHelper.detectPs()).toBe(true);
+      expect(mockConsoleError).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('test isMy', () => {
+    test('when locale is ms', () => {
+      const navigator = { language: 'ms' };
+
+      Object.defineProperty(global, 'navigator', { value: navigator });
+      jest.spyOn(Date.prototype, 'getTimezoneOffset').mockReturnValue(-480);
+      mockParse.mockReturnValue({ language: 'ms' });
+      expect(localeHelper.detectMy()).toBe(true);
+      expect(mockConsoleError).not.toHaveBeenCalled();
+    });
+
+    test('when locale is ms-MY', () => {
+      const navigator = { language: 'ms-MY' };
+
+      Object.defineProperty(global, 'navigator', { value: navigator });
+      jest.spyOn(Date.prototype, 'getTimezoneOffset').mockReturnValue(-480);
+      mockParse.mockReturnValue({ region: 'MY' });
+      expect(localeHelper.detectMy()).toBe(true);
+      expect(mockConsoleError).not.toHaveBeenCalled();
     });
   });
 });
