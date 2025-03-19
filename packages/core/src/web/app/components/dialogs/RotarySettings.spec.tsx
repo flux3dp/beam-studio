5de281fd954ad54f41d6182bc458e95bfa293dff
Dean Sung <dean@flux3dp.com>
2025-03-19 17:38:38 +0800

    Update Setting Modals

--- packages/core/src/web/app/components/dialogs/RotarySettings.spec.tsx
+++ packages/core/src/web/app/components/dialogs/RotarySettings.spec.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { act } from 'react';
 
 import { fireEvent, render } from '@testing-library/react';
 
@@ -62,24 +62,18 @@ describe('test RotarySettings', () => {
   test('disabled and enabled fields', () => {
     const { baseElement, getByText } = render(<RotarySettings onClose={mockOnClose} />);
     const segControl = baseElement.querySelector('#rotary_type');
+    const chuckDiameter = baseElement.querySelector('#object_diameter');
+    const circumference = baseElement.querySelector('#circumference');
+    const mirrorCheckbox = baseElement.querySelector('#mirror');
+    const extendCheckbox = baseElement.querySelector('#extend');
+    const scaleSelect = baseElement.querySelector('#scale');
 
     expect(segControl).toHaveClass('ant-segmented-disabled');
-
-    const chuckDiameter = baseElement.querySelector('#object_diameter');
-
     expect(chuckDiameter).toHaveAttribute('disabled');
-
-    const circumference = baseElement.querySelector('#circumference');
-
     expect(circumference).toHaveAttribute('disabled');
-
-    const mirrorCheckbox = baseElement.querySelector('#mirror');
-
     expect(mirrorCheckbox.parentNode).toHaveClass('ant-checkbox-disabled');
-
-    const extendCheckbox = baseElement.querySelector('#extend');
-
     expect(extendCheckbox.parentNode).toHaveClass('ant-checkbox-disabled');
+    expect(scaleSelect).toHaveAttribute('disabled');
 
     const rotaryMode = baseElement.querySelector('#rotary_mode');
 
@@ -89,6 +83,7 @@ describe('test RotarySettings', () => {
     expect(circumference).toHaveAttribute('disabled');
     expect(mirrorCheckbox.parentNode).not.toHaveClass('ant-checkbox-disabled');
     expect(extendCheckbox.parentNode).not.toHaveClass('ant-checkbox-disabled');
+    expect(scaleSelect).not.toHaveAttribute('disabled');
 
     const chuckSegmentSelector = getByText('Chuck');
 
@@ -100,30 +95,28 @@ describe('test RotarySettings', () => {
   test('save settings', () => {
     const { baseElement, getByText } = render(<RotarySettings onClose={mockOnClose} />);
     const rotaryMode = baseElement.querySelector('#rotary_mode');
+    const chuckSegmentSelector = getByText('Chuck');
+    const chuckDiameter = baseElement.querySelector('#object_diameter');
+    const extendCheckbox = baseElement.querySelector('#extend');
+    const scaleSelect = baseElement.querySelector('#scale');
 
     fireEvent.click(rotaryMode);
-
-    const chuckSegmentSelector = getByText('Chuck');
-
     fireEvent.click(chuckSegmentSelector);
-
-    const chuckDiameter = baseElement.querySelector('#object_diameter');
-
     fireEvent.change(chuckDiameter, { target: { value: 10 } });
-
-    const extendCheckbox = baseElement.querySelector('#extend');
-
     fireEvent.click(extendCheckbox);
+    act(() => fireEvent.mouseDown(scaleSelect));
+    fireEvent.click(baseElement.querySelector('.rc-virtual-list [title="2"]'));
 
     const saveButton = baseElement.querySelector('.ant-btn-primary');
 
     fireEvent.click(saveButton);
-    expect(mockWrite).toHaveBeenCalledTimes(5);
+    expect(mockWrite).toHaveBeenCalledTimes(6);
     expect(mockWrite).toHaveBeenNthCalledWith(1, 'rotary_mode', true);
     expect(mockWrite).toHaveBeenNthCalledWith(2, 'rotary-type', RotaryType.Chuck);
-    expect(mockWrite).toHaveBeenNthCalledWith(3, 'rotary-chuck-obj-d', 10);
-    expect(mockWrite).toHaveBeenNthCalledWith(4, 'rotary-mirror', false);
-    expect(mockWrite).toHaveBeenNthCalledWith(5, 'extend-rotary-workarea', true);
+    expect(mockWrite).toHaveBeenNthCalledWith(3, 'rotary-scale', 2);
+    expect(mockWrite).toHaveBeenNthCalledWith(4, 'rotary-chuck-obj-d', 10);
+    expect(mockWrite).toHaveBeenNthCalledWith(5, 'rotary-mirror', false);
+    expect(mockWrite).toHaveBeenNthCalledWith(6, 'extend-rotary-workarea', true);
     expect(mockChangeWorkarea).toHaveBeenCalledTimes(1);
     expect(mockChangeWorkarea).toHaveBeenLastCalledWith('ado1', { toggleModule: false });
     expect(mockToggleDisplay).toHaveBeenCalledTimes(1);
