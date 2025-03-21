a16d0c2133a89d72623f44780d58134f356c7366
Dean Sung <dean@flux3dp.com>
2025-03-21 18:34:02 +0800

    Fix tests

--- packages/core/src/web/helpers/layer/full-color/toggleFullColorLayer.ts
+++ packages/core/src/web/helpers/layer/full-color/toggleFullColorLayer.ts
@@ -1,39 +1,28 @@
+import history from '@core/app/svgedit/history/history';
+import undoManager from '@core/app/svgedit/history/undoManager';
 import updateLayerColor from '@core/helpers/color/updateLayerColor';
-import { getSVGAsync } from '@core/helpers/svg-editor-helper';
 import type { IBatchCommand } from '@core/interfaces/IHistory';
-import type ISVGCanvas from '@core/interfaces/ISVGCanvas';
 
-import { writeDataLayer } from '../layer-config-helper';
-
-let svgCanvas: ISVGCanvas;
-
-getSVGAsync((globalSVG) => {
-  svgCanvas = globalSVG.Canvas;
-});
+import { getData, writeDataLayer } from '../layer-config-helper';
 
 const toggleFullColorLayer = (layer: Element, opts: { val?: boolean } = {}): IBatchCommand | null => {
   const { val } = opts;
-  const origVal = layer.getAttribute('data-fullcolor') === '1';
+  const origVal = getData(layer, 'fullcolor');
   const targetVal = val ?? !origVal;
 
   console.log('Toggle Full Color Layer', layer, 'from', origVal, 'to', targetVal);
 
   if (targetVal === origVal) return null;
 
-  svgCanvas.undoMgr.beginUndoableChange('data-fullcolor', [layer]);
+  const cmd = new history.BatchCommand('Toggle Full Color Layer');
 
-  if (targetVal) {
-    writeDataLayer(layer, 'fullcolor', true);
-  } else {
-    writeDataLayer(layer, 'fullcolor', false);
-  }
-
-  const cmd = svgCanvas.undoMgr.finishUndoableChange();
+  writeDataLayer(layer, 'fullcolor', targetVal, { batchCmd: cmd });
 
   updateLayerColor(layer as SVGGElement);
   cmd.onAfter = () => {
     updateLayerColor(layer as SVGGElement);
   };
+  undoManager.addCommandToHistory(cmd);
 
   return cmd;
 };
