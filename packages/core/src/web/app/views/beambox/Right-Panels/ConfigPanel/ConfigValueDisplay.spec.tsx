b9afacbc8ac203aa7feffad896a48d0f4467be22
Dean Sung <dean@flux3dp.com>
2025-03-25 14:11:31 +0800

    Replace Input in ConfigValueDisplay

--- packages/core/src/web/app/views/beambox/Right-Panels/ConfigPanel/ConfigValueDisplay.spec.tsx
+++ packages/core/src/web/app/views/beambox/Right-Panels/ConfigPanel/ConfigValueDisplay.spec.tsx
@@ -4,51 +4,6 @@ import { fireEvent, render } from '@testing-library/react';
 
 import ConfigValueDisplay from './ConfigValueDisplay';
 
-jest.mock(
-  '@core/app/widgets/Unit-Input-v2',
-  () =>
-    ({ decimal, defaultValue, disabled, displayMultiValue, getValue, id, max, min, unit }: any) => (
-      <div>
-        MockUnitInput
-        <p>id: {id}</p>
-        <p>min: {min}</p>
-        <p>max: {max}</p>
-        <p>unit: {unit}</p>
-        <p>defaultValue: {defaultValue}</p>
-        <p>decimal: {decimal}</p>
-        <p>displayMultiValue: {displayMultiValue}</p>
-        <p>disabled: {disabled ? 'y' : 'n'}</p>
-        <button onClick={() => getValue(7)} type="button">
-          change
-        </button>
-      </div>
-    ),
-);
-
-jest.mock('antd', () => ({
-  ConfigProvider: ({ children, ...props }: any) => (
-    <div>
-      <p>Props: {JSON.stringify(props)}</p>
-      {children}
-    </div>
-  ),
-  InputNumber: ({ className, controls, max, min, onChange, precision, type, value }: any) => (
-    <div>
-      Mock InputNumber
-      <p>precision: {precision}</p>
-      <p>controls: {controls ? 'y' : 'n'}</p>
-      <input
-        className={className}
-        max={max}
-        min={min}
-        onChange={(e) => onChange(Number(e.target.value))}
-        type={type}
-        value={value}
-      />
-    </div>
-  ),
-}));
-
 describe('test ConfigValueDisplay when type is not panel-item', () => {
   it('should render correctly', () => {
     const { container } = render(
@@ -70,7 +25,7 @@ describe('test ConfigValueDisplay when type is not panel-item', () => {
 
   test('onChange should work', () => {
     const mockOnChange = jest.fn();
-    const { getByText } = render(
+    const { container } = render(
       <ConfigValueDisplay
         decimal={2}
         hasMultiValue={false}
@@ -83,8 +38,9 @@ describe('test ConfigValueDisplay when type is not panel-item', () => {
         value={5}
       />,
     );
+    const input = container.querySelector('input');
 
-    fireEvent.click(getByText('change'));
+    fireEvent.change(input, { target: { value: '7' } });
     expect(mockOnChange).toHaveBeenCalledTimes(1);
     expect(mockOnChange).toHaveBeenNthCalledWith(1, 7);
   });
