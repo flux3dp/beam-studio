02453d51711b9a6360bafc00b05e6a9b99391f6d
Tuhacrt <a067800497@gmail.com>
2025-04-01 18:11:29 +0800

    t

--- packages/core/src/web/app/components/BridgePanel/index.tsx
+++ packages/core/src/web/app/components/BridgePanel/index.tsx
@@ -3,13 +3,13 @@ import React, { memo, useCallback, useEffect, useRef, useState } from 'react';
 import { Flex } from 'antd';
 import paper from 'paper';
 
-import progressCaller from '@core/app/actions/progress-caller';
 import FullWindowPanel from '@core/app/widgets/FullWindowPanel/FullWindowPanel';
 import useNewShortcutsScope from '@core/helpers/hooks/useNewShortcutsScope';
 import shortcuts from '@core/helpers/shortcuts';
 import useForceUpdate from '@core/helpers/use-force-update';
 import useI18n from '@core/helpers/useI18n';
 
+import { useHistory } from './hooks/useHistory';
 import styles from './index.module.scss';
 import Sider from './Sider';
 import TopBar from './TopBar';
@@ -21,16 +21,22 @@ interface Props {
 }
 
 const PADDING = 30;
+const TARGET_PATH_NAME = 'parentPath';
 
 function UnmemorizedBridgePanel({ element, onClose }: Props): React.JSX.Element {
   const {
     beambox: { photo_edit_panel: langPhoto },
     image_edit_panel: lang,
   } = useI18n();
+  const { history, push, redo, set, undo } = useHistory({
+    hasUndid: false,
+    index: 0,
+    items: [{ pathData: [] }],
+  });
   const forceUpdate = useForceUpdate();
   const [mode, setMode] = useState<'auto' | 'manual'>('manual');
-  const [pathData, setPathData] = useState('');
-  const [offset, setOffset] = useState({ x: 0, y: 0 });
+  const [pathData, setPathData] = useState(Array.of<string>());
+  const [isPathDataChanged, setIsPathDataChanged] = useState(false);
   const [bridgeWidth, setBridgeWidth] = useState(10); // 0.5
   const [bridgeGap, setBridgeGap] = useState(10);
   // only for display percentage, not for calculation
@@ -39,16 +45,33 @@ function UnmemorizedBridgePanel({ element, onClose }: Props): React.JSX.Element 
   const divRef = useRef<HTMLDivElement>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
+  const handlePushHistory = useCallback(() => {
+    if (!isPathDataChanged) return;
+
+    push({ pathData });
+    setIsPathDataChanged(false);
+  }, [push, pathData, isPathDataChanged]);
+  const handleHistoryChange = useCallback(
+    (action: 'redo' | 'undo') => () => {
+      const { pathData } = action === 'undo' ? undo() : redo();
+
+      setPathData(pathData);
+      setIsPathDataChanged(false);
+      paper.view.requestUpdate();
+    },
+    [undo, redo],
+  );
+
   const handleReset = useCallback(() => {
     // handleZoom(fitScreenDimension.scale);
   }, [fitScreenDimension]);
 
   const handleComplete = useCallback(() => {
-    progressCaller.openNonstopProgress({ id: 'bridge-editing', message: langPhoto.processing });
+    // progressCaller.openNonstopProgress({ id: 'bridge-editing', message: langPhoto.processing });
 
     // add a frame to wait for re-render, otherwise the image might be blank or not updated
     requestAnimationFrame(() => {});
-  }, [langPhoto]);
+  }, []);
 
   const getPointerPosition = useCallback((event: paper.Event) => {
     const point = paper.view.getEventPoint(event);
@@ -63,19 +86,17 @@ function UnmemorizedBridgePanel({ element, onClose }: Props): React.JSX.Element 
       paper.project.hitTest(point, {
         fill: false,
         match: (hit: paper.HitResult) => {
-          console.log(hit.item.parent?.name);
-
           const circle = new paper.Path.Circle(point, 3);
 
           circle.fillColor = new paper.Color('red');
 
-          if (hit.item.parent?.name === 'parentPath') {
+          // Ensure the path is the target compound path's child
+          if (hit.item.parent?.name === TARGET_PATH_NAME) {
             const path = hit.item as paper.Path;
             const newCompoundPath = cutPathAtDistance(path, point, bridgeWidth);
 
-            console.log(newCompoundPath.pathData);
-
-            setPathData(newCompoundPath.pathData);
+            setPathData((prev) => prev.concat(newCompoundPath.pathData) || [newCompoundPath.pathData]);
+            setIsPathDataChanged(true);
           }
 
           return true;
@@ -86,10 +107,6 @@ function UnmemorizedBridgePanel({ element, onClose }: Props): React.JSX.Element 
     },
     [bridgeWidth],
   );
-
-  const handleMouseMove = useCallback((event: paper.MouseEvent) => {
-    // console.log(event.point);
-  }, []);
 
   useEffect(() => {
     const inputPathData = element.getAttribute('d');
@@ -108,24 +125,12 @@ function UnmemorizedBridgePanel({ element, onClose }: Props): React.JSX.Element 
         bounds: { center, height, width },
       } = originalPath;
       const scaleFactor = Math.min(1, (clientWidth - PADDING * 2) / width, (clientHeight - PADDING * 2) / height);
-
-      console.log(center, height, width, scaleFactor);
-
       const newCenter = new paper.Point(
         Math.max(PADDING, width * scaleFactor),
         Math.max(PADDING, height * scaleFactor),
       );
 
-      // paper.view.center = newCenter;
-
-      // console.log(newCenter, paper.view.center);
-      // paper.view.translate(newCenter);
-      // const canvasSize = Math.min(paper.view.size.width, paper.view.size.height);
-      // const targetSize = canvasSize * 0.8;
-      // const scaleFactor = Math.min(targetSize / size.width, targetSize / size.height);
-
-      // Store offset and scale for later
-      setOffset({ x: center.x, y: center.y });
+      setFitScreenDimension({ scale: scaleFactor, x: newCenter.x, y: newCenter.y });
       setZoomScale(scaleFactor);
 
       // Create normalized path
@@ -137,21 +142,18 @@ function UnmemorizedBridgePanel({ element, onClose }: Props): React.JSX.Element 
       normalizedPath.position = newCenter;
       normalizedPath.strokeColor = new paper.Color('black');
       normalizedPath.strokeWidth = 1;
-      normalizedPath.name = 'parentPath';
+      normalizedPath.name = TARGET_PATH_NAME;
 
       const circle = new paper.Path.Circle(newCenter, 3);
 
       circle.fillColor = new paper.Color('red');
 
       paper.view.center = newCenter;
+      // paper.view.zoom = scaleFactor;
 
       // Store normalized path data
-      setPathData(normalizedPath.pathData);
-
-      originalPath.remove();
-
-      // Zoom to fit
-      paper.view.zoom = 1;
+      setPathData([normalizedPath.pathData]);
+      set({ pathData: [normalizedPath.pathData] });
     };
 
     initialize();
@@ -183,15 +185,23 @@ function UnmemorizedBridgePanel({ element, onClose }: Props): React.JSX.Element 
     const { tool } = paper;
 
     tool.onMouseDown = handleMouseDown;
-    tool.onMouseMove = handleMouseMove;
-  }, [handleMouseDown, handleMouseMove]);
+    tool.onMouseUp = handlePushHistory;
+  }, [handleMouseDown, handlePushHistory]);
+
+  useEffect(() => {
+    if (paper.project.isEmpty()) return;
+
+    const path = paper.project.getItem({ name: TARGET_PATH_NAME }) as paper.CompoundPath;
+
+    path.pathData = pathData.at(-1)!;
+  }, [pathData]);
 
   useNewShortcutsScope();
   useEffect(() => {
     const subscribedShortcuts = [
       shortcuts.on(['Escape'], onClose, { isBlocking: true }),
-      // shortcuts.on(['Fnkey+z'], handleHistoryChange('undo'), { isBlocking: true }),
-      // shortcuts.on(['Shift+Fnkey+z'], handleHistoryChange('redo'), { isBlocking: true }),
+      shortcuts.on(['Fnkey+z'], handleHistoryChange('undo'), { isBlocking: true }),
+      shortcuts.on(['Shift+Fnkey+z'], handleHistoryChange('redo'), { isBlocking: true }),
       // shortcuts.on(['Fnkey-+', 'Fnkey-='], () => handleZoomByScale(1.2), {
       //   isBlocking: true,
       //   splitKey: '-',
@@ -202,7 +212,7 @@ function UnmemorizedBridgePanel({ element, onClose }: Props): React.JSX.Element 
     return () => {
       subscribedShortcuts.forEach((unsubscribe) => unsubscribe());
     };
-  }, [onClose]);
+  }, [handleHistoryChange, onClose]);
 
   return (
     <FullWindowPanel
@@ -222,10 +232,10 @@ function UnmemorizedBridgePanel({ element, onClose }: Props): React.JSX.Element 
           />
           <Flex className={styles['w-100']} vertical>
             <TopBar
-              handleHistoryChange={(() => {}) as any}
+              handleHistoryChange={handleHistoryChange}
               handleReset={handleReset}
               handleZoomByScale={(() => {}) as any}
-              history={{ index: 0, items: [] }}
+              history={history}
               zoomScale={zoomScale}
             />
             <div className={styles['outer-container']}>
