2dfc290197f5a8fcb9fe7aa4b23078e19caf847f
Dean Sung <dean@flux3dp.com>
2025-04-14 15:36:34 +0800

    Remove debug codes

--- packages/core/src/web/helpers/api/ai-extension.ts
+++ packages/core/src/web/helpers/api/ai-extension.ts
@@ -8,13 +8,6 @@ import initState from '@core/app/views/beambox/Right-Panels/ConfigPanel/initStat
 import { writeData } from '@core/helpers/layer/layer-config-helper';
 import Websocket from '@core/helpers/websocket';
 
-const mockData = {
-  layerData:
-    '{"\u5716\u5C64_1":{"id":"L0.73519897460938","name":"\u5716\u5C64_1","color":{"red":78.6926070038911,"green":127.501945525292,"blue":255,"typename":"RGBColor"},"power":90,"speed":150}}',
-  status: 'ok',
-  svg: '<?xml version="1.0" encoding="iso-8859-1"?>\r\n<!-- Generator: Adobe Illustrator 29.4.0, SVG Export Plug-In . SVG Version: 9.03 Build 0)  -->\r\n<svg version="1.1" id="&#x5716;&#x5C64;_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"\r\n\t y="0px" viewBox="0 0 595.276 841.89" enable-background="new 0 0 595.276 841.89" xml:space="preserve">\r\n<rect x="230.438" y="256.208" fill="#FFFFFF" stroke="#000000" stroke-miterlimit="10" width="108.333" height="131.25"/>\r\n<rect x="310.645" y="330.167" fill="#FFFFFF" stroke="#000000" stroke-miterlimit="10" width="108.334" height="131.25"/>\r\n<rect x="252.586" y="414.542" fill="#FFFFFF" stroke="#000000" stroke-miterlimit="10" width="90.104" height="145.833"/>\r\n</svg>\r\n',
-};
-
 const init = (): { connection: any } => {
   const events = {
     onError: (response: any) => {
@@ -67,8 +60,6 @@ const init = (): { connection: any } => {
   tabController.onFocused(() => {
     ws?.send('set_handler');
   });
-
-  window.testAIExt = () => events.onMessage(mockData);
 
   return {
     connection: ws,
