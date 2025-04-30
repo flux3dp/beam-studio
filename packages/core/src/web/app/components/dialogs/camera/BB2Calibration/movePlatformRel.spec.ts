572c176b6057553e7be76d095420a3e549a9fbd3
Dean Sung <dean@flux3dp.com>
2025-04-30 11:55:40 +0800

    Add unittest for movePlatformRel

---
+++ packages/core/src/web/app/components/dialogs/camera/BB2Calibration/movePlatformRel.spec.ts
@@ -0,0 +1,51 @@
+import movePlatformRel from './movePlatformRel';
+
+const mockEnterRawMode = jest.fn();
+const mockRawMoveZRel = jest.fn();
+const mockEndSubTask = jest.fn();
+const mockGetCurrentControlMode = jest.fn();
+const mockSetTimeout = jest.spyOn(global, 'setTimeout');
+
+jest.mock('@core/helpers/device-master', () => ({
+  get currentControlMode() {
+    return mockGetCurrentControlMode();
+  },
+  endSubTask: (...args: any) => mockEndSubTask(...args),
+  enterRawMode: (...args: any) => mockEnterRawMode(...args),
+  rawMoveZRel: (...args: any) => mockRawMoveZRel(...args),
+}));
+
+describe('test movePlatformRel', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    mockGetCurrentControlMode.mockReturnValue('');
+    mockSetTimeout.mockImplementation((cb) => {
+      cb();
+
+      return 0 as unknown as NodeJS.Timeout;
+    });
+  });
+
+  it('should work without error', async () => {
+    await movePlatformRel(10);
+    expect(mockEnterRawMode).toHaveBeenCalledTimes(1);
+    expect(mockRawMoveZRel).toHaveBeenCalledTimes(1);
+    expect(mockEndSubTask).toHaveBeenCalledTimes(1);
+    expect(mockSetTimeout).toHaveBeenCalledTimes(1);
+  });
+
+  it('should exit raw mode if error occurs', async () => {
+    mockGetCurrentControlMode.mockReturnValue('raw');
+
+    const error = new Error('test error');
+
+    mockRawMoveZRel.mockRejectedValue(error);
+    try {
+      await movePlatformRel(10);
+    } catch (e) {
+      expect(e).toEqual(error);
+    }
+    expect(mockSetTimeout).not.toHaveBeenCalled();
+    expect(mockEndSubTask).toHaveBeenCalledTimes(1);
+  });
+});
