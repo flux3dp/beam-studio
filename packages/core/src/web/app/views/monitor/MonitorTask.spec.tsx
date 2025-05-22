eaf56335bcb7ec56141fbe4bd6d72f3185419363
EstherFu <sloborfu@gmail.com>
2025-05-22 13:01:35 +0800

    Fix tests

--- packages/core/src/web/app/views/monitor/MonitorTask.spec.tsx
+++ packages/core/src/web/app/views/monitor/MonitorTask.spec.tsx
@@ -20,20 +20,21 @@ jest.mock('./MonitorControl', () => () => <div>Dummy MonitorControl</div>);
 jest.mock('@core/helpers/device/framing', () => ({
   __esModule: true,
   default: jest.fn().mockImplementation(() => ({
+    destroy: jest.fn(),
     on: jest.fn(),
     startFraming: jest.fn(),
     stopFraming: jest.fn(),
   })),
+  framingOptions: {
+    Framing: {
+      description: 'framing_desc',
+      title: 'framing',
+    },
+  },
   FramingType: {
     Framing: 'Framing',
   },
   getFramingOptions: () => ['Framing'],
-}));
-
-// Mock MessageCaller
-jest.mock('@core/app/actions/message-caller', () => ({
-  closeMessage: jest.fn(),
-  openMessage: jest.fn(),
 }));
 
 describe('should render correctly', () => {
@@ -70,6 +71,8 @@ describe('should render correctly', () => {
   });
 
   it('should render correctly when completed', () => {
+    formatDuration.mockReturnValue('1m 30s');
+
     const { container } = render(
       <MonitorContext.Provider
         value={
@@ -81,12 +84,64 @@ describe('should render correctly', () => {
               st_id: 64,
             },
             taskImageURL: 'img/flux.svg',
-            totalTaskTime: 0,
+            totalTaskTime: 90,
             uploadProgress: null,
           } as any
         }
       >
         <MonitorTask device={{ name: 'device' } as any} />
+      </MonitorContext.Provider>,
+    );
+
+    expect(container).toMatchSnapshot();
+    expect(formatDuration).toHaveBeenCalledTimes(1);
+    expect(formatDuration).toHaveBeenNthCalledWith(1, 90);
+  });
+
+  it('should render correctly for Promark', () => {
+    formatDuration.mockReturnValue('1m 30s');
+
+    const { container } = render(
+      <MonitorContext.Provider
+        value={
+          {
+            fileInfo: ['filename'],
+            mode: Mode.WORKING,
+            report: {
+              prog: 0,
+              st_id: 64,
+            },
+            taskImageURL: 'img/flux.svg',
+            totalTaskTime: 90,
+            uploadProgress: null,
+          } as any
+        }
+      >
+        <MonitorTask device={{ model: 'fpm1', name: 'device' } as any} />
+      </MonitorContext.Provider>,
+    );
+
+    expect(container).toMatchSnapshot();
+    expect(formatDuration).toHaveBeenCalledTimes(1);
+    expect(formatDuration).toHaveBeenNthCalledWith(1, 90);
+  });
+
+  it('should render correctly for Promark without task data', () => {
+    const { container } = render(
+      <MonitorContext.Provider
+        value={
+          {
+            mode: Mode.WORKING,
+            report: {
+              prog: 0,
+              st_id: 64,
+            },
+            totalTaskTime: 0,
+            uploadProgress: null,
+          } as any
+        }
+      >
+        <MonitorTask device={{ model: 'fpm1', name: 'device' } as any} />
       </MonitorContext.Provider>,
     );
 
