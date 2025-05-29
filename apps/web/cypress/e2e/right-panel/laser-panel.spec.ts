00ce59c80737374fa3bba4c7f20c1bff395cadf6
Tuhacrt <a067800497@gmail.com>
2025-05-29 18:49:16 +0800

    test: update tests

--- apps/web/cypress/e2e/right-panel/laser-panel.spec.ts
+++ apps/web/cypress/e2e/right-panel/laser-panel.spec.ts
@@ -232,8 +232,8 @@ describe('manipulate laser panel', () => {
     cy.wait(5000);
     cy.readFile(cypressDownloadPath)
       .its('presets')
-      .should('have.length', 62)
-      .its('61')
+      .should('have.length', 63)
+      .its('62')
       .its('name')
       .should('eq', 'Hi Flux');
   });
