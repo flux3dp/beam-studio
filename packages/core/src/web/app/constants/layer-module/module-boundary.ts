ab98e19131f39c09723c70c9608e1cffa6064482
Dean Sung <dean@flux3dp.com>
2025-06-09 15:46:34 +0800

    Temp

--- packages/core/src/web/app/constants/layer-module/module-boundary.ts
+++ packages/core/src/web/app/constants/layer-module/module-boundary.ts
@@ -1,9 +1,15 @@
+import type { LayerModuleType } from '@core/app/constants/layer-module/layer-modules';
 import { LayerModule } from '@core/app/constants/layer-module/layer-modules';
+import type { WorkAreaModel } from '@core/app/constants/workarea-constants';
+
+const moduleBoundaries: Partial<
+  Record<WorkAreaModel, Partial<Record<LayerModuleType, { bottom: number; left: number; right: number; top: number }>>>
+> = {};
 
 // Module Boundary for ador
 // bottom boundary is added by 20mm for 1064nm laser
 const moduleBoundary: {
-  [model: number]: { bottom: number; left: number; right: number; top: number };
+  [model: LayerModuleType]: { bottom: number; left: number; right: number; top: number };
 } = {
   [LayerModule.LASER_10W_DIODE]: { bottom: 20, left: 0, right: 0, top: 0 },
   [LayerModule.LASER_20W_DIODE]: { bottom: 30, left: 0, right: 0, top: 0 },
