48439ffa22e2da03d41039d702dc990f3fec318b
Tuhacrt <a067800497@gmail.com>
2025-06-12 18:07:10 +0800

    test: fix e2e

--- packages/core/src/web/helpers/clipper/offset/constants.ts
+++ packages/core/src/web/helpers/clipper/offset/constants.ts
@@ -7,7 +7,7 @@ export type OffsetMode =
 export const SCALE_FACTOR = 100; // Scale factor for ClipperLib operations, to handle precision issues
 export const ROUND_FACTOR = 100; // Used for rounding points in fitPath
 export const UNSUPPORTED_TAGS = ['g', 'image', 'text', 'use'] as const; // Tags that are not supported for offset operations
-export const MITER_LIMIT = 1; // Miter limit for ClipperOffset
+export const MITER_LIMIT = 5; // Miter limit for ClipperOffset
 export const ARC_TOLERANCE = 0.25; // Arc tolerance for ClipperOffset
 
 type Point = { X: number; Y: number };
