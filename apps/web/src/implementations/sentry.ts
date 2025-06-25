1cf4b9facb1ac2a6e84d41346a23afe4866bc443
Dean Sung <dean@flux3dp.com>
2025-06-25 11:56:35 +0800

    Update sentry release

--- apps/web/src/implementations/sentry.ts
+++ apps/web/src/implementations/sentry.ts
@@ -5,7 +5,7 @@ const initSentry = (): void => {
   Sentry.init({
     dsn: 'https://bbd96134db9147658677dcf024ae5a83@o28957.ingest.sentry.io/5617300',
     integrations: [new Integrations.BrowserTracing()],
-    release: 'Beam-Studio-Web@1.0.0',
+    release: 'Beam-Studio-Web@2.5.6',
     // Set tracesSampleRate to 1.0 to capture 100%
     // of transactions for performance monitoring.
     // We recommend adjusting this value in production
