b53356a5ddf77ce8defdd0954c9dd511e6c42298
EstherFu <sloborfu@gmail.com>
2025-07-04 17:41:12 +0800

    Fix tests

--- packages/core/src/__mocks__/@core/helpers/checkFeature.ts
+++ packages/core/src/__mocks__/@core/helpers/checkFeature.ts
@@ -1,3 +1,4 @@
 export const checkFpm1 = (): boolean => true;
 export const checkHxRf = (): boolean => true;
 export const checkBM2 = (): boolean => true;
+export const checkBM2UV = (): boolean => true;
