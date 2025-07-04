ecc4f1966900b31d4900b46bc62a87226de7c05d
EstherFu <sloborfu@gmail.com>
2025-07-04 18:17:54 +0800

    save

--- packages/core/src/web/app/components/welcome/TabFollowUs.tsx
+++ packages/core/src/web/app/components/welcome/TabFollowUs.tsx
@@ -1,9 +1,14 @@
 import { FacebookOutlined, LikeOutlined, LinkedinOutlined, PinterestFilled } from '@ant-design/icons';
 import { Button } from 'antd';
 
+import { getSocialMedia } from '@core/app/constants/social-media-constants';
+import browser from '@core/implementations/browser';
+
 import styles from './TabFollowUs.module.scss';
 
 const TabFollowUs = () => {
+  const socialMedia = getSocialMedia();
+
   return (
     <div>
       <div className={styles.title}>
@@ -12,11 +17,11 @@ const TabFollowUs = () => {
       <div className={styles.subtitle}>Get inspirations, deals, freebies and engage with FLUXers!</div>
       <div className={styles.content}>
         <div className={styles.imgs}>
-          <div>
-            <img src="https://s3-alpha-sig.figma.com/img/1da3/b73f/598ebe941cb7ede5560b57358c45cddf?Expires=1745193600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=fMfsWyLaKaDMmaB-T1riqKIQwN-WzhNlRyDJZ7MPHaIgpooRHrROcMkH7KXuCXo4RSMUg5hFqvkY~WzylCcJsRv5-S4L9-qm3q-8Gtt3TLlg572~RF-wo1Xxr-esZ8i9Q4wxtK74vyE2nwlH~GzWYVHww6wtwV-JD7yTMWI4urIZVLSokwjVnpDbjjiV~IvfEZMUWN5E3d49gwtEwew8uQt1J1cELb6d7y6tLayN6oYscphPz79~~hLMG6tDWhgyefnkrpzNNzA89XhxijikNC60Z5ZoaSMAFWA~2QeBmnUZnITfwM822~tfTo00PYyPBmRmzuYqOFDGtPiSaqXfpw__" />
+          <div onClick={() => browser.open(socialMedia.youtube.link)}>
+            <img src={socialMedia.youtube.subscribeSrc} />
           </div>
-          <div>
-            <img src="https://s3-alpha-sig.figma.com/img/1da3/b73f/598ebe941cb7ede5560b57358c45cddf?Expires=1745193600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=fMfsWyLaKaDMmaB-T1riqKIQwN-WzhNlRyDJZ7MPHaIgpooRHrROcMkH7KXuCXo4RSMUg5hFqvkY~WzylCcJsRv5-S4L9-qm3q-8Gtt3TLlg572~RF-wo1Xxr-esZ8i9Q4wxtK74vyE2nwlH~GzWYVHww6wtwV-JD7yTMWI4urIZVLSokwjVnpDbjjiV~IvfEZMUWN5E3d49gwtEwew8uQt1J1cELb6d7y6tLayN6oYscphPz79~~hLMG6tDWhgyefnkrpzNNzA89XhxijikNC60Z5ZoaSMAFWA~2QeBmnUZnITfwM822~tfTo00PYyPBmRmzuYqOFDGtPiSaqXfpw__" />
+          <div onClick={() => browser.open(socialMedia.instagram.link)}>
+            <img src={socialMedia.instagram.subscribeSrc} />
           </div>
         </div>
         <div className={styles.buttons}>
--- packages/core/src/web/app/constants/social-media-constants.ts
+++ packages/core/src/web/app/constants/social-media-constants.ts
@@ -15,11 +15,13 @@ export const getSocialMedia = () => {
       link: isTW ? 'https://www.instagram.com/fluxinctaiwan/' : 'https://www.instagram.com/flux_inc/',
       name: 'Instagram',
       src: `core-img/social-media/instagram-${langKey}.png`,
+      subscribeSrc: `core-img/social-media/subscribe-instagram-${langKey}.png`,
     },
     youtube: {
       link: isTW ? 'https://www.youtube.com/@FLUXIncTaiwan' : 'https://www.youtube.com/@fluxinc',
       name: 'YouTube',
       src: `core-img/social-media/youtube-${langKey}.png`,
+      subscribeSrc: `core-img/social-media/subscribe-youtube-${langKey}.png`,
     },
   };
 };
