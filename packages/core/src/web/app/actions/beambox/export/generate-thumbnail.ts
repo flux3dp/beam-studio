1f654ed8166012c092a81c003f3c6da940bf075f
EstherFu <sloborfu@gmail.com>
2025-07-21 18:00:30 +0800

    Crop task thumbnail

--- packages/core/src/web/app/actions/beambox/export/generate-thumbnail.ts
+++ packages/core/src/web/app/actions/beambox/export/generate-thumbnail.ts
@@ -1,3 +1,5 @@
+import beamboxPreference from '@core/app/actions/beambox/beambox-preference';
+import workareaManager from '@core/app/svgedit/workarea';
 import { getSVGAsync } from '@core/helpers/svg-editor-helper';
 
 const { $ } = window;
@@ -42,20 +44,55 @@ const fetchThumbnail = async (): Promise<string[]> => {
     const canvas = document.createElement('canvas');
     const ctx = canvas.getContext('2d')!;
 
-    // cropping
-    const ratio = img.width / $('#svgroot').width();
-    const W = ratio * $('#svgroot').width();
-    const H = ratio * $('#svgroot').height();
-    const w = ratio * Number.parseFloat($('#canvasBackground').attr('width'));
-    const h = ratio * Number.parseFloat($('#canvasBackground').attr('height'));
-    const offsetY = ratio * Number.parseFloat($('#canvasBackgroundRect').attr('y'));
-    const x = -(W - w) / 2;
-    const y = -(H - h) / 2 - offsetY;
+    if (beamboxPreference.read('crop-task-thumbnail')) {
+      const { maxY, minY, width } = workareaManager;
+      let x = Number.parseFloat($('#svgcontent').attr('x'));
+      let y = Number.parseFloat($('#svgcontent').attr('y'));
+      let w = Number.parseFloat($('#svgcontent').attr('width'));
+      const ratio = w / width;
+      const svgContent = document.getElementById('svgcontent') as unknown as SVGSVGElement;
+      const bbox = svgContent.getBBox();
+      const padding = 100;
+      let right = bbox.x + bbox.width + padding;
+      let bottom = bbox.y + bbox.height + padding;
 
-    canvas.width = Math.min(w, 500);
-    canvas.height = Math.ceil(h * (canvas.width / w));
+      bbox.x -= padding;
+      bbox.y -= padding;
 
-    ctx.drawImage(img, -x, -y, w, h, 0, 0, canvas.width, canvas.height);
+      if (bbox.x < 0) bbox.x = 0;
+
+      if (right > width) right = width;
+
+      if (bbox.y < minY) bbox.y = minY;
+
+      if (bottom > maxY) bottom = maxY;
+
+      bbox.width = right - bbox.x;
+      bbox.height = bottom - bbox.y;
+      x += bbox.x * ratio;
+      y += bbox.y * ratio;
+      w = bbox.width * ratio;
+
+      const h = bbox.height * ratio;
+
+      canvas.width = Math.min(w, 500);
+      canvas.height = Math.ceil(h * (canvas.width / w));
+      ctx.drawImage(img, x, y, w, h, 0, 0, canvas.width, canvas.height);
+    } else {
+      const ratio = img.width / $('#svgroot').width();
+      const W = ratio * $('#svgroot').width();
+      const H = ratio * $('#svgroot').height();
+      const w = ratio * Number.parseFloat($('#canvasBackground').attr('width'));
+      const h = ratio * Number.parseFloat($('#canvasBackground').attr('height'));
+      const offsetY = ratio * Number.parseFloat($('#canvasBackgroundRect').attr('y'));
+      const x = -(W - w) / 2;
+      const y = -(H - h) / 2 - offsetY;
+
+      canvas.width = Math.min(w, 500);
+      canvas.height = Math.ceil(h * (canvas.width / w));
+
+      ctx.drawImage(img, -x, -y, w, h, 0, 0, canvas.width, canvas.height);
+    }
 
     return canvas;
   }
