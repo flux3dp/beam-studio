0e5f075447ebad4c8db5d17cd14f2354b03e5a44
Tuhacrt <a067800497@gmail.com>
2025-07-24 11:12:13 +0800

    fix: text-on-path element rotation

--- packages/core/src/web/helpers/convertToImage/types.ts
+++ packages/core/src/web/helpers/convertToImage/types.ts
@@ -11,3 +11,16 @@ export type BBox = { height: number; width: number; x: number; y: number };
 export type CreateImageParams = Record<'angle' | 'height' | 'width' | 'x' | 'y', number> &
   Record<'href' | 'transform', string>;
 export type MainConverterFunc = (params: ConvertSvgToImageParams) => Promise<ConvertToImageResult>;
+
+export const convertibleSvgTags = [
+  'rect',
+  'circle',
+  'ellipse',
+  'line',
+  'polygon',
+  'polyline',
+  'path',
+  'text',
+  'use',
+  'g',
+] as const;
