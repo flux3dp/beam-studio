64c1388fa0abbac2fae951490c267c63ee74073a
Dean Sung <dean@flux3dp.com>
2025-07-31 13:48:29 +0800

    Add timeout for openFile for cloud

--- packages/core/src/web/helpers/api/cloudFile.ts
+++ packages/core/src/web/helpers/api/cloudFile.ts
@@ -107,6 +107,7 @@ export const openFile = async (file: IFile): Promise<OperationResult<Blob>> => {
   try {
     const resp = await axiosFluxId.get(`/api/beam-studio/cloud/file/${file.uuid}`, {
       responseType: 'blob',
+      timeout: 120000,
       withCredentials: true,
     });
     const checkRespResult = await checkResp(resp);
