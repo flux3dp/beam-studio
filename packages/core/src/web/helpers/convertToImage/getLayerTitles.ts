6af0369c4686506fba3b3b94860f086e4ed7e9eb
Tuhacrt <a067800497@gmail.com>
2025-08-05 17:23:04 +0800

    fix: revise according to comments

--- packages/core/src/web/helpers/convertToImage/getLayerTitles.ts
+++ packages/core/src/web/helpers/convertToImage/getLayerTitles.ts
@@ -4,7 +4,7 @@ export const getLayerTitles = (svgElement: SVGElement): string[] => {
   const titles: string[] = [];
   const elements: SVGElement[] = [svgElement];
   const getLayerTitle = (element: SVGElement) => {
-    if (element.tagName === 'g') {
+    if (element.getAttribute('data-tempgroup') === 'true') {
       elements.push(...(element.children as unknown as SVGElement[]));
 
       return;
