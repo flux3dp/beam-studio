65b7eadf414b7f6a733b1a323016e2fb14fe1bb3
Tuhacrt <a067800497@gmail.com>
2025-08-07 15:24:41 +0800

    feat(app): click file to open app now can read file after setup

--- apps/app/src/init-backend-events.ts
+++ apps/app/src/init-backend-events.ts
@@ -1,9 +1,9 @@
 import communicator from '@core/implementations/communicator';
 
 const initBackendEvents = (): void => {
-  communicator.on('BACKEND_UP', (_: any, status: { alive: boolean; logfile: any; port: number }) => {
+  communicator.on('BACKEND_UP', (_: any, status: { alive: boolean; logFile: any; port: number }) => {
     window.FLUX.ghostPort = status.port;
-    window.FLUX.logfile = status.logfile;
+    window.FLUX.logFile = status.logFile;
     window.FLUX.backendAlive = status.alive;
     console.log(`Backend start at ${status.port}`);
   });
@@ -14,10 +14,10 @@ const initBackendEvents = (): void => {
 
   communicator.on(
     'NOTIFY_BACKEND_STATUS',
-    (_: any, status: { backend: { alive: boolean; logfile: any; port: number } }) => {
+    (_: any, status: { backend: { alive: boolean; logFile: any; port: number } }) => {
       console.log(status);
       window.FLUX.ghostPort = status.backend.port;
-      window.FLUX.logfile = status.backend.logfile;
+      window.FLUX.logFile = status.backend.logFile;
       window.FLUX.backendAlive = status.backend.alive;
 
       if (status.backend.alive) {
