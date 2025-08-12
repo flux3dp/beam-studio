987736f71ddcefd008f8cb5424e30794a1735ad6
Dean Sung <dean@flux3dp.com>
2025-08-12 15:41:58 +0800

    Update change log to 2.5.8 (#583)
    * Change zh-tw change logs
    
    * Update en change log

--- apps/app/src/implementations/changelog.ts
+++ apps/app/src/implementations/changelog.ts
@@ -1,40 +1,43 @@
 import type { IChangeLog } from '@core/interfaces/IChangeLog';
 
-// 2.5.7 beta
+// 2.5.8 beta
 const CHANGES_TW = {
-  added: [
-    '新增 Beam Studio 首頁。',
-    '新增「圓角」功能於「編輯圖片」。',
-    '新增 AF 自動對焦按鈕（僅限 Ador、Beambox II、HEXA）。',
-    '新增 支援向量及點陣圖物件執行位移複製。',
-    '新增 「內容裁切縮圖」選項於偏好設定。',
-    '新增 「使用最大範圍」選項於偏好設定。',
-  ],
+  added: ['新增 向量圖的向量轉點陣圖功能。'],
   changed: [
-    '變更 AI 去背功能的點數計算方式。',
-    '變更 RTL 文字會自動適用文字轉路徑 2.0。',
-    '變更「非工作區域」的樣式與行為。',
+    '變更 部分文件設定應套用於新分頁。',
+    '變更 文字可直接跟路徑執行合併功能。',
+    '變更 對齊相關功能需考慮非工作區域。',
+    '變更 旋轉軸軸心初始顯示的座標。',
+    '變更 使用當前位置時不設旋轉軸心。',
+    '變更 連線至機器 IP 時的錯誤訊息。',
+    '變更 點陣圖的銳化功能視窗。',
+    '變更 複製貼上行為。',
   ],
-  fixed: ['修正 文字路徑轉為路徑後消失的問題。', '修正 封閉路徑補償為 0 時，路徑雕刻順序錯誤的問題。'],
+  fixed: [
+    '修正 旋轉物件經過廣域雕刻後，部分內容無法正常顯示的問題。',
+    '修正 部分偏好設定在新開分頁沒有正確套用的問題。',
+    '修正 v2.5.7 雙擊 beam 檔時沒有正確開啟檔案的問題。',
+    '修正 上傳大型檔案至雲端時的錯誤。',
+  ],
 };
 
 const CHANGES_EN = {
-  added: [
-    'Added Beam Studio Home page.',
-    'Added "Rounded Corners" feature in "Edit Image".',
-    'Added AF (Auto Focus) button (Ador, Beambox II, and HEXA only, HEXA requires firmware v4.3.11 or later).',
-    'Added offset feature for vector and raster objects.',
-    'Added "Content Crop Thumbnail" option in Preferences.',
-    'Added "Use Maximum Range" option in Preferences.',
-  ],
+  added: ['Added Convert to Image function for vector objects.'],
   changed: [
-    'Changed the credit calculation method for the AI Background Removal feature.',
-    'RTL text will now automatically use Text-to-Path converter 2.0.',
-    'Changed the interface and behavior of the "Non-working area"',
+    'Changed some document settings to now apply to new tabs.',
+    'Changed text can now be directly united with paths.',
+    'Changed alignment functions to take non-work areas into account.',
+    'Changed the initial coordinates for rotation axis center display.',
+    'Cancelled setting the rotation axis center when using the current position.',
+    'Changed error messages shown when unable to connect to machine by IP.',
+    'Changed the Sharpen tool window for images.',
+    'Changed the behavior for copy and paste.',
   ],
   fixed: [
-    'Fixed an issue where a text-on-path object will disappear after being converted to a path.',
-    'Fixed incorrect engraving order when loop compensation is set to 0.',
+    'Fixed an issue where rotated objects were not fully displayed after using the Passthrough function.',
+    'Fixed an issue where some preferences were not correctly applied in new tabs.',
+    'Fixed an issue in v2.5.7 where double-clicking a .beam file did not open it correctly.',
+    'Fixed an error when uploading large files to the My Cloud.',
   ],
 };
 
