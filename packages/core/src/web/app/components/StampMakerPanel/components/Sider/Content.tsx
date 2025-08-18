a8a2df4ebd8354bc461aa524e38b589d08f417ee
Tuhacrt <a067800497@gmail.com>
2025-08-18 15:32:41 +0800

    feat: add stamp-maker-panel

---
+++ packages/core/src/web/app/components/StampMakerPanel/components/Sider/Content.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import { QuestionCircleOutlined } from '@ant-design/icons';
+import { Flex, Form, InputNumber, Slider } from 'antd';
+
+import useI18n from '@core/helpers/useI18n';
+
+import styles from './PanelContent.module.scss';
+
+const MAX_BRUSH_SIZE = 128;
+
+export default function Eraser(): React.JSX.Element {
+  const { image_edit_panel: lang } = useI18n();
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.title}>{lang.eraser.title}</div>
+      <div className={styles['hint-text']}>
+        <QuestionCircleOutlined className={styles.icon} />
+        <span>{lang.eraser.description}</span>
+      </div>
+      <Form layout="vertical">
+        <Form.Item label={`${lang.eraser.brush_size}:`}>
+          <Flex align="center" gap={8} justify="between">
+            <Slider className={styles.slider} max={MAX_BRUSH_SIZE} min={1} onChange={() => {}} step={1} value={20} />
+            <InputNumber
+              max={MAX_BRUSH_SIZE}
+              min={1}
+              onChange={(val) => {
+                if (val !== null) return;
+              }}
+              step={1}
+              value={20}
+            />
+          </Flex>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+}
---
+++ packages/core/src/web/app/components/StampMakerPanel/components/Sider/index.tsx
@@ -0,0 +1,53 @@
+import React, { memo } from 'react';
+
+import type { TabPaneProps } from 'antd';
+import { Button, ConfigProvider, Flex } from 'antd';
+
+import ImageEditPanelIcons from '@core/app/icons/image-edit-panel/ImageEditPanelIcons';
+import BackButton from '@core/app/widgets/FullWindowPanel/BackButton';
+import Footer from '@core/app/widgets/FullWindowPanel/Footer';
+import Header from '@core/app/widgets/FullWindowPanel/Header';
+import FullWindowPanelSider from '@core/app/widgets/FullWindowPanel/Sider';
+import useI18n from '@core/helpers/useI18n';
+
+import Content from './Eraser';
+import styles from './index.module.scss';
+
+export type Mode = 'cornerRadius' | 'eraser' | 'magicWand';
+
+interface Props {
+  handleComplete: () => void;
+  onClose: () => void;
+}
+
+interface Tab extends Omit<TabPaneProps, 'tab'> {
+  key: string;
+  label: React.ReactNode;
+}
+
+function Sider({ handleComplete, onClose }: Props): React.JSX.Element {
+  const { buttons: langButtons, global: langGlobal, image_edit_panel: lang } = useI18n();
+
+  return (
+    <ConfigProvider theme={{ components: { InputNumber: { controlWidth: 80 } } }}>
+      <FullWindowPanelSider className={styles.sider}>
+        <Flex className={styles['h-100']} justify="space-between" vertical>
+          <div>
+            <BackButton onClose={onClose}>{langButtons.back_to_beam_studio}</BackButton>
+            <Header icon={<ImageEditPanelIcons.EditImage />} title={lang.title} />
+            <Content />
+          </div>
+          <Footer>
+            <Button key="ok" onClick={handleComplete} type="primary">
+              {langGlobal.ok}
+            </Button>
+          </Footer>
+        </Flex>
+      </FullWindowPanelSider>
+    </ConfigProvider>
+  );
+}
+
+const MemorizedSider = memo(Sider);
+
+export default MemorizedSider;
