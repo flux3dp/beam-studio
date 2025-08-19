0ba75c04541af64dd582c0ce718fff5e73ab32af
Dean Sung <dean@flux3dp.com>
2025-08-19 10:31:25 +0800

    Fix Path.tsx

--- packages/core/src/web/app/components/settings/Path.tsx
+++ packages/core/src/web/app/components/settings/Path.tsx
@@ -7,6 +7,7 @@ import SettingUnitInput from '@core/app/components/settings/components/SettingUn
 import XYItem from '@core/app/components/settings/components/XYItem';
 import { getWorkarea } from '@core/app/constants/workarea-constants';
 import { useSettingStore } from '@core/app/pages/Settings/useSettingStore';
+import { useStorageStore } from '@core/app/stores/storageStore';
 import useI18n from '@core/helpers/useI18n';
 
 import SettingFormItem from './components/SettingFormItem';
@@ -19,6 +20,7 @@ type Props = {
 };
 
 const Path = ({ options, unitInputProps }: Props): React.JSX.Element => {
+  const activeLang = useStorageStore((state) => state['active-lang']);
   const lang = useI18n();
   const { getConfig, getPreference, setConfig, setPreference } = useSettingStore();
 
@@ -50,7 +52,7 @@ const Path = ({ options, unitInputProps }: Props): React.JSX.Element => {
           value={getConfig('loop_compensation') / 10}
         />
       </SettingFormItem>
-      {i18n.getActiveLang() === 'zh-cn' ? (
+      {activeLang === 'zh-cn' ? (
         <div>
           <SettingFormItem id="set_blade_radius" label={lang.settings.blade_radius}>
             <SettingUnitInput
