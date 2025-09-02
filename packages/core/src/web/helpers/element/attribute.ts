e0eec2cfb92fd9eee7b5f5dbf6a7a3658ccf31fa
EstherFu <95398751+EstherFu@users.noreply.github.com>
2025-09-02 16:38:35 +0800

    Fix fill color in printer layers (#615)

---
+++ packages/core/src/web/helpers/element/attribute.ts
@@ -0,0 +1,31 @@
+type AttributeMap = Record<string, string>;
+
+const defaultAttributes: AttributeMap = {
+  'data-ratiofixed': 'false',
+  fill: '#000000',
+  'fill-opacity': '1',
+  stroke: 'none',
+  'stroke-width': '1',
+};
+
+export const getAttributeWithDefault = (elem: Element, attribute: string): string => {
+  return elem.getAttribute(attribute) ?? defaultAttributes[attribute];
+};
+
+export const getAttributes = (elem: Element, attributes: string[]): AttributeMap => {
+  return attributes.reduce((acc, attr) => {
+    acc[attr] = getAttributeWithDefault(elem, attr);
+
+    return acc;
+  }, {} as AttributeMap);
+};
+
+export const setAttributes = (elem: Element, attributes: Record<string, null | string>): void => {
+  Object.entries(attributes).forEach(([attr, value]) => {
+    if (value === null) {
+      elem.removeAttribute(attr);
+    } else {
+      elem.setAttribute(attr, value);
+    }
+  });
+};
