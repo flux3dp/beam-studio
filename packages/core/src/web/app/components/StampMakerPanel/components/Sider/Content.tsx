5f9fe8b483bc9545c4fff3601f09da257bb671ca
Tuhacrt <a067800497@gmail.com>
2025-09-03 11:15:51 +0800

    feat: remove tooltip, add hint link

--- packages/core/src/web/app/components/StampMakerPanel/components/Sider/Content.tsx
+++ packages/core/src/web/app/components/StampMakerPanel/components/Sider/Content.tsx
@@ -1,11 +1,11 @@
 import React, { useMemo } from 'react';
 
-import { QuestionCircleOutlined } from '@ant-design/icons';
-import { Flex, Form, Switch, Tooltip } from 'antd';
+import { Flex, Form, Switch } from 'antd';
 import Konva from 'konva';
 
 import UnitInput from '@core/app/widgets/UnitInput';
 import useI18n from '@core/helpers/useI18n';
+import browser from '@core/implementations/browser';
 
 import { useStampMakerPanelStore } from '../../store';
 
@@ -22,26 +22,22 @@ export default function Content(): React.JSX.Element {
   return (
     <div className={styles.wrapper}>
       <Form layout="horizontal">
+        <div className={styles.link} onClick={() => browser.open(stamp_maker_panel.how_to.url)}>
+          {stamp_maker_panel.how_to.text}
+        </div>
         <Form.Item label={stamp_maker_panel.invert}>
           <Flex align="center" gap={8}>
             <Switch checked={isInverted} onChange={toggleInvert} />
-            <Tooltip title={stamp_maker_panel.tool_tip.invert}>
-              <QuestionCircleOutlined className={styles['tool-tip']} />
-            </Tooltip>
           </Flex>
         </Form.Item>
         <Form.Item label={stamp_maker_panel.horizontal_flip}>
           <Flex align="center" gap={8}>
             <Switch checked={horizontalFlip} onChange={handleToggleFlip} />
-            <Tooltip title={stamp_maker_panel.tool_tip.horizontal_flip}>
-              <QuestionCircleOutlined className={styles['tool-tip']} />
-            </Tooltip>
           </Flex>
         </Form.Item>
         <Form.Item label={stamp_maker_panel.bevel_radius} layout="vertical">
           <Flex justify="space-between">
             <UnitInput
-              //
               addonAfter="mm"
               max={10}
               min={0}
@@ -50,6 +46,7 @@ export default function Content(): React.JSX.Element {
                   setBevelRadius(value);
                 }
               }}
+              precision={1}
               step={0.1}
               value={bevelRadius}
             />
