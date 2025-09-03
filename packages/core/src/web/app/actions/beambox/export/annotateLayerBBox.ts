73bb3688be44b4ada0d374d1eb8a58335cb7a88b
Dean Sung <dean@flux3dp.com>
2025-09-03 15:48:37 +0800

    Fix annotateLayerBBox boundary condition

--- packages/core/src/web/app/actions/beambox/export/annotateLayerBBox.ts
+++ packages/core/src/web/app/actions/beambox/export/annotateLayerBBox.ts
@@ -6,7 +6,7 @@ import { getAllLayers } from '@core/helpers/layer/layer-helper';
 
 export const annotateLayerBBox = (): (() => void) => {
   const allLayers = getAllLayers();
-  const { minY } = workareaManager;
+  const { height, minY, width } = workareaManager;
 
   allLayers.forEach((layer) => {
     const repeat = getData(layer, 'repeat');
@@ -16,12 +16,14 @@ export const annotateLayerBBox = (): (() => void) => {
     if (ref || repeat === 0 || (layerModule && layerModule < 0)) return;
 
     const bbox = layer.getBBox();
+    const right = Math.min(bbox.x + bbox.width, width);
+    const bottom = Math.min(bbox.y + bbox.height, minY + height);
     const x = Math.max(bbox.x, 0);
-    const y = Math.max(bbox.y - minY, 0);
-    const w = Math.min(bbox.width, workareaManager.width);
-    const h = Math.min(bbox.height, workareaManager.height);
+    const y = Math.max(bbox.y, minY);
+    const w = right - x;
+    const h = bottom - y;
 
-    layer.setAttribute('data-bbox', `${round(x, 2)},${round(y, 2)},${round(w, 2)},${round(h, 2)}`);
+    layer.setAttribute('data-bbox', `${round(x, 2)},${round(y - minY, 2)},${round(w, 2)},${round(h, 2)}`);
   });
 
   const revert = () => allLayers.forEach((layer) => layer.removeAttribute('data-bbox'));
