97adff3f66b064a8f3fce94e428e60a55d15d9f2
Dean Sung <dean@flux3dp.com>
2025-09-05 15:15:18 +0800

    Sync more i18n

--- packages/core/src/web/helpers/device-error-handler.ts
+++ packages/core/src/web/helpers/device-error-handler.ts
@@ -1,9 +1,9 @@
 import i18n from '@core/helpers/i18n';
 
-const { lang } = i18n;
-
 export default {
   translate: (error: Record<string, unknown> | string | string[]): string => {
+    const { lang } = i18n;
+
     // When error is object but not array
     if (typeof error === 'object' && !Array.isArray(error)) {
       return JSON.stringify(error);
@@ -16,12 +16,12 @@ export default {
     error = Array.isArray(error) ? error : [error];
 
     if (error.length) {
-      if (lang.generic_error[error[0]]) {
-        return lang.generic_error[error[0]];
+      if (lang.generic_error[error[0] as keyof typeof lang.generic_error]) {
+        return lang.generic_error[error[0] as keyof typeof lang.generic_error];
       }
 
       for (let i = error.length; i >= 1; i -= 1) {
-        errorOutput = lang.monitor[error.slice(0, i).join('_')];
+        errorOutput = lang.monitor[error.slice(0, i).join('_') as keyof typeof lang.monitor] as string;
 
         if (errorOutput) {
           break;
