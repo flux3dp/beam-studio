214ad17ed5de821de3e1606f19ef06cd45da10b8
EstherFu <sloborfu@gmail.com>
2025-09-16 11:45:36 +0800

    Fix tutorial position

--- packages/core/src/web/app/constants/layout-constants.ts
+++ packages/core/src/web/app/constants/layout-constants.ts
@@ -8,6 +8,7 @@ export default {
   rulerWidth: 15, // px
   sidePanelsWidth: window.os !== 'MacOS' ? 308 : 292, // px
   titlebarHeight: window.os === 'Windows' && !isWeb() ? 30 : 0, // px
+  toolButtonHeight: 43, // px, 30 px button + 13 px margin
   topBarHeight: window.os === 'Windows' && !isWeb() ? 70 : 40, // px
   topBarHeightWithoutTitleBar: 40, // px
 };
