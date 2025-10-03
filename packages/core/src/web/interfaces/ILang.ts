958b3dcbffbd215592f5bca4258dcdadf785228a
Dean Sung <dean@flux3dp.com>
2025-10-03 16:54:37 +0800

    Refactor firmware update, add mainboard firmware upload

--- packages/core/src/web/interfaces/ILang.ts
+++ packages/core/src/web/interfaces/ILang.ts
@@ -2358,7 +2358,8 @@ export interface ILang {
       too_old_for_web: string;
       update_fail: string;
       update_success: string;
-      upload_file: string;
+      upload_firmware_title: string;
+      upload_mainboard_title: string;
     };
     install: string;
     later: string;
