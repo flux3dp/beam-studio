79542a01345f24ac71f3d2b672e701eebbea3363
Dean Sung <dean@flux3dp.com>
2025-12-23 11:00:13 +0800

    Add check bm2 by region

--- packages/core/src/__mocks__/@core/helpers/checkFeature.ts
+++ packages/core/src/__mocks__/@core/helpers/checkFeature.ts
@@ -1,4 +1,5 @@
 export const checkFpm1 = (): boolean => true;
 export const checkHxRf = (): boolean => true;
+export const checkBM2 = (): boolean => true;
 export const checkBM2UV = (): boolean => true;
 export const checkBM2CurveEngraving = (): boolean => true;
