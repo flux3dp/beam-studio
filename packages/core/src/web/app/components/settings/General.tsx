a137238e04d1db5d33c4fa1ef8f3e6fecbc4ce15
Tuhacrt <a067800497@gmail.com>
2025-12-31 15:15:12 +0800

    feat: change display of general module in settings-modal

--- packages/core/src/web/app/components/settings/General.tsx
+++ packages/core/src/web/app/components/settings/General.tsx
@@ -6,34 +6,52 @@ import i18n from '@core/helpers/i18n';
 import isWeb from '@core/helpers/is-web';
 import useI18n from '@core/helpers/useI18n';
 
+import SettingsCard from './components/SettingsCard';
 import SettingSelect from './components/SettingSelect';
 
 interface Props {
   changeActiveLang: (val: string) => void;
   supportedLangs: Record<string, string>;
+  wrapped?: boolean;
 }
 
-function General({ changeActiveLang, supportedLangs }: Props): React.JSX.Element {
+function General({ changeActiveLang, supportedLangs, wrapped = false }: Props): React.JSX.Element {
   const lang = useI18n();
   const { getConfig, setConfig } = useSettingStore();
   const langOptions = Object.keys(supportedLangs).map((value) => ({ label: supportedLangs[value], value }));
 
+  const Wrapper = wrapped ? SettingsCard : React.Fragment;
+
   return (
     <>
-      <SettingSelect
-        defaultValue={i18n.getActiveLang()}
-        id="select-lang"
-        label={lang.settings.language}
-        onChange={changeActiveLang}
-        options={langOptions}
-      />
       {!isWeb() && (
-        <SettingSwitch
-          checked={getConfig('notification')}
-          id="set-notifications"
-          label={lang.settings.notifications}
-          onChange={(e) => setConfig('notification', e)}
+        <Wrapper>
+          <SettingSwitch
+            checked={getConfig('auto_check_update')}
+            id="set-auto-update"
+            label={lang.settings.check_updates}
+            onChange={(e) => setConfig('auto_check_update', e)}
+          />
+        </Wrapper>
+      )}
+      <Wrapper>
+        <SettingSelect
+          defaultValue={i18n.getActiveLang()}
+          id="select-lang"
+          label={lang.settings.language}
+          onChange={changeActiveLang}
+          options={langOptions}
         />
+      </Wrapper>
+      {!isWeb() && (
+        <Wrapper>
+          <SettingSwitch
+            checked={getConfig('notification')}
+            id="set-notifications"
+            label={lang.settings.notifications}
+            onChange={(e) => setConfig('notification', e)}
+          />
+        </Wrapper>
       )}
     </>
   );
