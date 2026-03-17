0ca5f96495c9253655f1956000056276c22b1587
EstherFu <sloborfu@gmail.com>
2026-03-17 18:12:51 +0800

    Remove deprecated app e2e

--- apps/app/test/features/editor/top-panels-tset/document-test.js
+++
@@ -1,9 +0,0 @@
-const { checkExist } = require('../../../util/utils');
-
-test('Change Speed', async function() {
-    const { app } = require('../../../test');
-    await checkExist('#svgcanvas',15000);
-    await app.client.execute(() => {
-        Dialog.showDocumentSettings()
-    });
-});
