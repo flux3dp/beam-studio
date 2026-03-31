2a0aaaa5aa26e1f891d028391c3db91041ff0c11
Dean Sung <dean@flux3dp.com>
2026-03-31 15:03:30 +0800

    Add KeysWithType

--- packages/core/src/web/interfaces/utils/index.ts
+++ packages/core/src/web/interfaces/utils/index.ts
@@ -4,3 +4,8 @@ export type OneOf<T1 extends Record<string, any>, T2 extends Record<string, any>
   | Prettify<Partial<T1> & T2>
   | Prettify<Partial<T2> & T1>;
 // the other is required
+
+/** Extract keys of T whose values extend V */
+export type KeysWithType<T, V> = {
+  [K in keyof T]: T[K] extends V ? K : never;
+}[keyof T];
