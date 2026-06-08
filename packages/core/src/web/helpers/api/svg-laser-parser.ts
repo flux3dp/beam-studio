565f3cd2e421cd7ce0ebad4582938eae6188b9a1
EstherFu <sloborfu@gmail.com>
2026-06-08 11:47:38 +0800

    Fix

--- packages/core/src/web/helpers/api/svg-laser-parser.ts
+++ packages/core/src/web/helpers/api/svg-laser-parser.ts
@@ -329,6 +329,7 @@ export const getExportOpt = async (
       'jump_speed',
       'jump_delay_min',
       'jump_delay_max',
+      'is_uv_light',
     ];
     let storageValue: null | string = null;
 
