6432f814eed0edb997ccd37f1ed04c2299e1751b
Dean Sung <dean@flux3dp.com>
2026-06-12 18:47:22 +0800

    Enable bm2 for more region (#896)
    * Update i18n
    
    * Enable bm2 for PS and IL

--- packages/core/src/web/helpers/checkFeature.ts
+++ packages/core/src/web/helpers/checkFeature.ts
@@ -6,7 +6,8 @@ const enableAllMachines = window?.localStorage?.getItem('enableAllMachines') ===
 
 export const checkFpm1 = (): boolean => !isWeb();
 export const checkHxRf = (): boolean => enableAllMachines || isDev() || localeHelper.isTwOrHk;
-export const checkBM2 = (): boolean => enableAllMachines || isDev() || localeHelper.isTwOrHk || localeHelper.isJp;
+export const checkBM2 = (): boolean =>
+  enableAllMachines || isDev() || localeHelper.isTwOrHk || localeHelper.isJp || localeHelper.isPs || localeHelper.isIl;
 // TODO: Can be removed after we move all testing machine to fuv1
 export const checkBM2UV = (): boolean => isDev();
 export const checkBM2CurveEngraving = (): boolean => isDev();
