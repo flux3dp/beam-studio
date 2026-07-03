f575bf25a71521048403cfe0055bf1c16546a1ea
Dean Sung <dean@flux3dp.com>
2026-07-03 10:30:35 +0800

    Update checkHxRf (#918)

--- packages/core/src/web/helpers/checkFeature.ts
+++ packages/core/src/web/helpers/checkFeature.ts
@@ -5,7 +5,20 @@ import localeHelper from './locale-helper';
 const enableAllMachines = window?.localStorage?.getItem('enableAllMachines') === 'true';
 
 export const checkFpm1 = (): boolean => !isWeb();
-export const checkHxRf = (): boolean => enableAllMachines || isDev() || localeHelper.isTwOrHk;
+export const checkHxRf = (): boolean => {
+  if (enableAllMachines || isDev() || localeHelper.isTwOrHk || localeHelper.isJp) return true;
+
+  const now = Date.now();
+
+  // Staged rollout: EU from 2026/8/4, USA from 2026/8/15, all users from 2026/9/1
+  if (now >= new Date('2026/9/1').valueOf()) return true;
+
+  if (localeHelper.isEu && now >= new Date('2026/8/4').valueOf()) return true;
+
+  if (localeHelper.isNorthAmerica && now >= new Date('2026/8/15').valueOf()) return true;
+
+  return false;
+};
 export const checkBM2 = (): boolean =>
   enableAllMachines || isDev() || localeHelper.isTwOrHk || localeHelper.isJp || localeHelper.isPs || localeHelper.isIl;
 // TODO: Can be removed after we move all testing machine to fuv1
