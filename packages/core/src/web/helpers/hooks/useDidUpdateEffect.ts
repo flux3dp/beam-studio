9d1356be9f75d17c6efd194d7fd0f6580b8b4d7e
Dean Sung <dean@flux3dp.com>
2026-07-08 16:29:35 +0800

    Update useDidUpdateEffect

--- packages/core/src/web/helpers/hooks/useDidUpdateEffect.ts
+++ packages/core/src/web/helpers/hooks/useDidUpdateEffect.ts
@@ -1,16 +1,20 @@
+import type { DependencyList, EffectCallback } from 'react';
 import { useEffect, useRef } from 'react';
 
 // work like componentDidUpdate
 // ref: https://stackoverflow.com/questions/53253940/make-react-useeffect-hook-not-run-on-initial-render
-const useDidUpdateEffect = (effect: () => void, deps: any[]) => {
+const useDidUpdateEffect = (effect: EffectCallback, deps: DependencyList) => {
   const didMount = useRef(false);
 
   useEffect(() => {
-    if (didMount.current) {
-      effect();
-    } else {
+    if (!didMount.current) {
       didMount.current = true;
+
+      return undefined;
     }
+
+    // Forward the effect's cleanup so callers can return one (e.g. clearTimeout).
+    return effect();
     // eslint-disable-next-line hooks/exhaustive-deps
   }, deps);
 };
