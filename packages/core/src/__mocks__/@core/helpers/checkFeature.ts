d777ddfe211037753124cb910796a041deac57e2
EstherFu <esther@flux3dp.com>
2026-08-12 18:01:01 +0800

    Show 4C in tw/hw only

--- packages/core/src/__mocks__/@core/helpers/checkFeature.ts
+++ packages/core/src/__mocks__/@core/helpers/checkFeature.ts
@@ -1,6 +1,7 @@
 export const checkFpm1 = (): boolean => true;
 export const checkHxRf = (): boolean => true;
 export const checkBM2 = (): boolean => true;
+export const checkBM24C = (): boolean => true;
 export const checkBM2UV = (): boolean => true;
 export const checkBM2CurveEngraving = (): boolean => true;
 export const checkFUV1 = (): boolean => true;
