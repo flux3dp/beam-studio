9256728555406996af3aea697333e252a47439fb
Dean Sung <dean@flux3dp.com>
2026-09-07 18:42:11 +0800

    Add checkHxRf back

--- packages/core/src/web/helpers/checkFeature.ts
+++ packages/core/src/web/helpers/checkFeature.ts
@@ -5,6 +5,7 @@ import localeHelper from './locale-helper';
 const enableAllMachines = window?.localStorage?.getItem('enableAllMachines') === 'true';
 
 export const checkFpm1 = (): boolean => !isWeb();
+export const checkHxRf = (): boolean => true;
 export const checkBM2 = (): boolean => true;
 export const checkBM24C = (): boolean => isDev() || localeHelper.isTwOrHk;
 // TODO: Can be removed after we move all testing machine to fuv1
