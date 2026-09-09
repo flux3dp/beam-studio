13fae8def932d4c58586fc07c9c054ac8ec8baa0
Dean Sung <dean@flux3dp.com>
2026-09-09 12:23:26 +0800

    Select cutting layer after generate

--- packages/core/src/web/app/components/dialogs/PrintAndCut/utils/generateCutLayer.ts
+++ packages/core/src/web/app/components/dialogs/PrintAndCut/utils/generateCutLayer.ts
@@ -188,6 +188,7 @@ export const generateAlignedCutLayer = (): void => {
   });
 
   layerManager.resync();
+  layerManager.setSelectedLayers([newLayerName]);
   selectionManager.clearSelection();
 
   if (!batchCmd.isEmpty()) undoManager.addCommandToHistory(batchCmd);
