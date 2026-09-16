41898770ca23cd1aa13aad9291af57ec1ceaa831
Dean Sung <dean@flux3dp.com>
2026-09-16 14:46:53 +0800

    Remove noun project from export_svg_warning

--- packages/core/src/web/app/lang/zh-cn.ts
+++ packages/core/src/web/app/lang/zh-cn.ts
@@ -2273,7 +2273,7 @@ const lang: ILang = {
     elements: '元素',
     enjoy_shape_library: '新增百万图形资源，请尽情使用。',
     export_svg_title: '无法汇出 SVG',
-    export_svg_warning: '专案中含有受知识产权法律的保护的Noun Project 物件，因此汇出时Beam Studio 会自动帮您排除这类型之物件，您仍可以透过储存场景(.beam 档) 的方式保留您的专案，请问是否要继续汇出？',
+    export_svg_warning: '专案中含有受知识产权法律的保护的元素物件，因此汇出时Beam Studio 会自动帮您排除这类型之物件，您仍可以透过储存场景(.beam 档) 的方式保留您的专案，请问是否要继续汇出？',
     learn_more: '更多资讯',
     login_first: '请先登入会员，完成后即可启用。',
     recent: '最近使用',
