39a244a3a783a6c764c3e1538637dceae88ba4e7
Dean Sung <dean@flux3dp.com>
2026-09-16 17:36:20 +0800

    Update message (#1001)
    * Update message
    
    * Remove noun project from export_svg_warning

--- packages/core/src/web/app/lang/it.ts
+++ packages/core/src/web/app/lang/it.ts
@@ -893,7 +893,7 @@ const lang: ILang = {
           disabled_by_gradient: 'Disattiva la sfumatura prima di utilizzare questa funzione.',
           disabled_by_infilled: 'Chiudi il riempimento prima di utilizzare questa funzione.',
           disabled_by_infilled_and_variable_text: 'Chiudi il riempimento e il testo variabile prima di utilizzare questa funzione.',
-          disabled_by_noun_project: 'Le forme del Noun Project sono protette da copyright, quindi i loro tracciati non possono essere modificati.',
+          disabled_by_noun_project: 'Questa forma è protetta dalla legge sulla proprietà intellettuale, quindi il suo tracciato non può essere modificato.',
           disabled_by_variable_text: 'Chiudi il testo variabile prima di utilizzare questa funzione.',
           disassemble_use: 'Disassembla',
           disassembling: 'Disassemblaggio in corso...',
